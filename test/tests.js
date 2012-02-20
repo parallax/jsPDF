2ba15a4d6928ce09beb24ee8668cf120cb65fe22
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-02-20 04:08:19 -0800

    Changed test runner to reuse code and loop over test runnables.

--- test/tests.js
+++ test/tests.js
@@ -5,22 +5,43 @@ var datestringregex = /\/CreationDate \(D:\d+\)/
 , removeMinorDiffs = function(t){
 	return t.replace(datestringregex, replacementdatestring).trim()
 }
-
-asyncTest("001_blankpdf", function() {
-	//QUnit.stop()
-	require(['text!001_blankpdf.txt'])
-	.then(function(thetext){
+, testinventory = {
+	"001_blankpdf.txt": function(){
 		var doc = new jsPDF()
-		, t1 = removeMinorDiffs( doc.output() ) 
-		, t2 = removeMinorDiffs( thetext )
-				
-		QUnit.expect(1)
-		QUnit.equal(
-			t1,t2
-		)
-		QUnit.start()
-		//stop()
+		return doc.output()
+	}
+	, "002_twopagedoc.txt":function(){
+		var doc = new jsPDF();
+		doc.text(20, 20, 'Hello world!');
+		doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+		doc.addPage();
+		doc.text(20, 20, 'Do you like that?');
+		return doc.output()
+	} 
+}
+, testrunner = function(reference_file_name, test_data_yielder){
+	asyncTest(reference_file_name, function() {
+		//QUnit.stop()
+		require(['text!'+reference_file_name])
+		.then(function(expectedtext){					
+			QUnit.expect(1)
+			QUnit.equal(
+				removeMinorDiffs( expectedtext )
+				, removeMinorDiffs( test_data_yielder() )
+			)
+			QUnit.start()
+			//stop()
+		})
 	})
-})
+}
+
+for (var filename in testinventory){
+	if (testinventory.hasOwnProperty(filename)){
+		testrunner(
+			filename
+			, testinventory[filename]
+		)
+	}
+}
 
 }) // end of document.ready(
