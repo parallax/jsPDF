9256948dad2fff5e793c405cc3c36d4ea2b8531c
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-02-22 04:03:19 -0800

    build catches up with reality

--- tools/minify.bat
+++ tools/minify.bat
@@ -1,4 +1,4 @@
 @echo off
 set rootdir=%~dp0..\
 
-java -jar /bin/closure_compiler.jar --js=%rootdir%\jspdf.js --js_output_file=%rootdir%\jspdf.min.js
+java -jar /bin/closure_compiler.jar --js=%rootdir%\jspdf.js --js=%rootdir%\libs\base64.js --js_output_file=%rootdir%\jspdf.min.js
