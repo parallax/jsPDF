ea1100270fd2ca5f5e1f9b9e6e7002de004a6c5b
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-06-11 04:21:00 -0700

    Switching to Wak-based build script. (from Windows-specific batch scripts)

--- tools/builddocumentation.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set rootdir=%~dp0..\
-set jsdocbindir=\bin\jsdoc-toolkit
-
-java -jar %jsdocbindir%\jsrun.jar %jsdocbindir%\app\run.js -v %rootdir%\jspdf.js -d=%rootdir%\doc -t=%rootdir%\tools\jsdoc_template
--- tools/minify.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set rootdir=%~dp0..\
-
-java -jar /bin/closure_compiler.jar --js=%rootdir%\jspdf.js --js_output_file=%rootdir%\jspdf.min.js
