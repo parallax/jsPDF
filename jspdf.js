671626fc9ad30bad695cb053049c099e39d86dde
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-06-19 15:33:34 -0700

    Yanking out UTF8 encoding out of base64 code
    To avoid doing it again, adding long note about it.
    
    PDf is always just an 8bit stream. All binary streams within PDF, even UCS-2BE Unicode text, are just that - 8-bit binary streams that PDF reader knows how to reassemble back into 16bit char arrays. base64 can handle raw PDF as is. If you think you need to UTF8 your pdf, you are doing PDF wrong.
    
    Besides, the utf8 encoder code had a bug. If you ever need a good JavaScript native String to utf8 encoder / decoder see here: http://ecmanaut.blogspot.com/2006/07/encoding-decoding-utf8-in-javascript.html

--- jspdf.js
+++ jspdf.js
@@ -88,29 +88,6 @@ function jsPDF(/** String */ orientation, /** String */ unit, /** String */ form
 	/////////////////////
 	// Private functions
 	/////////////////////
-	// Convert an internal javascript utf16 string into a sequence of bytes
-	// code from utf8.js http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
-	// public domain lisence
-	, utf16to8 = function(str) {
-		var out, i, len, c;
-
-		out = "";
-		len = str.length;
-		for(i = 0; i < len; i++) {
-			c = str.charCodeAt(i);
-			if ((c >= 0x0001) && (c <= 0x007F)) {
-				out += str.charAt(i);
-			} else if (c > 0x07FF) {
-				out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
-				out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
-				out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
-			} else {
-				out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
-				out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
-			}
-		}
-		return out;
-	}
 	// takes a string imgData containing the raw bytes of
 	// a jpeg image and returns [width, height]
 	// Algorithm from: http://www.64lines.com/jpeg-width-height
@@ -811,67 +788,89 @@ function jsPDF(/** String */ orientation, /** String */ unit, /** String */ form
 			out(id.toString(10) + ' j')
 		},
 		base64encode: function(data) {
+			// DO NOT ADD UTF8 ENCODING CODE HERE!!!!
+
+			// UTF8 encoding encodes bytes over char code 128
+			// and, essentially, turns an 8-bit binary streams
+			// (that base64 can deal with) into 7-bit binary streams. 
+			// (by default server does not know that and does not recode the data back to 8bit)
+			// You destroy your data.
+
+			// binary streams like jpeg image data etc, while stored in JavaScript strings,
+			// (which are 16bit arrays) are in 8bit format already.
+			// You do NOT need to char-encode that before base64 encoding.
+
+			// if you, by act of fate
+			// have string which has individual characters with code
+			// above 255 (pure unicode chars), encode that BEFORE you base64 here.
+			// you can use absolutely any approch there, as long as in the end,
+			// base64 gets an 8bit (char codes 0 - 255) stream.
+			// when you get it on the server after un-base64, you must 
+			// UNencode it too, to get back to 16, 32bit or whatever original bin stream.
+
+			// Note, Yes, JavaScript strings are, in most cases UCS-2 - 
+			// 16-bit character arrays. This does not mean, however,
+			// that you always have to UTF8 it before base64.
+			// it means that if you have actual characters anywhere in
+			// that string that have char code above 255, you need to
+			// recode *entire* string from 16-bit (or 32bit) to 8-bit array.
+			// You can do binary split to UTF16 (BE or LE)
+			// you can do utf8, you can split the thing by hand and prepend BOM to it,
+			// but whatever you do, make sure you mirror the opposite on
+			// the server. If server does not expect to post-process un-base64
+			// 8-bit binary stream, think very very hard about messing around with encoding.
+
+			// so, long story short:
+			// DO NOT ADD UTF8 ENCODING CODE HERE!!!!
+
 			// use native code if it's present
-			var encode = btoa || function(data) {
-				/** @preserve
-				====================================================================
-				base64 encoder
-				MIT, GPL
+		    if (typeof btoa === 'function') return btoa(data)
+		    
+			/** @preserve
+			====================================================================
+			base64 encoder
+			MIT, GPL
+		
+			version: 1109.2015
+			discuss at: http://phpjs.org/functions/base64_encode
+			+   original by: Tyler Akins (http://rumkin.com)
+			+   improved by: Bayron Guevara
+			+   improved by: Thunder.m
+			+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+			+   bugfixed by: Pellentesque Malesuada
+			+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+			+   improved by: Rafal Kukawski (http://kukawski.pl)
+			====================================================================
+			*/
+		    
+		    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+		    , b64a = b64.split('')
+		    , o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+	        ac = 0,
+	        enc = "",
+	        tmp_arr = [];
+		 
+		    do { // pack three octets into four hexets
+		        o1 = data.charCodeAt(i++);
+		        o2 = data.charCodeAt(i++);
+		        o3 = data.charCodeAt(i++);
+		 
+		        bits = o1 << 16 | o2 << 8 | o3;
 
-				version: 1109.2015
-				discuss at: http://phpjs.org/functions/base64_encode
-				+   original by: Tyler Akins (http://rumkin.com)
-				+   improved by: Bayron Guevara
-				+   improved by: Thunder.m
-				+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-				+   bugfixed by: Pellentesque Malesuada
-				+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-				+   improved by: Rafal Kukawski (http://kukawski.pl)
-				====================================================================
-				*/
+		        h1 = bits >> 18 & 0x3f;
+		        h2 = bits >> 12 & 0x3f;
+		        h3 = bits >> 6 & 0x3f;
+		        h4 = bits & 0x3f;
+		 
+		        // use hexets to index into b64, and append result to encoded string
+		        tmp_arr[ac++] = b64a[h1] + b64a[h2] + b64a[h3] + b64a[h4];
+		    } while (i < data.length);
 
-				var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-				, b64a = b64.split('')
-				, o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
-				ac = 0,
-				enc = "",
-				tmp_arr = [];
+		    enc = tmp_arr.join('');
+		    var r = data.length % 3;
+		    return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
 
-				do { // pack three octets into four hexets
-					o1 = data.charCodeAt(i++);
-					o2 = data.charCodeAt(i++);
-					o3 = data.charCodeAt(i++);
-
-					bits = o1 << 16 | o2 << 8 | o3;
-
-					h1 = bits >> 18 & 0x3f;
-					h2 = bits >> 12 & 0x3f;
-					h3 = bits >> 6 & 0x3f;
-					h4 = bits & 0x3f;
-
-					// use hexets to index into b64, and append result to encoded string
-					tmp_arr[ac++] = b64a[h1] + b64a[h2] + b64a[h3] + b64a[h4];
-				} while (i < data.length);
-
-				enc = tmp_arr.join('');
-				var r = data.length % 3;
-				return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
-
-				// end of base64 encoder MIT, GPL
-			}
-
-			try {
-				return encode(data)
-			} catch(e) {
-				// INVALID_CHARACTER_ERR
-				// This is what happens if we have utf16 characters in our string.
-				// If so, we should re-encode them as utf-8 bytes.
-				if (e.code === 5) {
-					return encode(utf16to8(data))
-				} else {
-					throw e
-				}
-			}
+		    // end of base64 encoder MIT, GPL
 		},
 		output: function(type, options) {
 			var undef
