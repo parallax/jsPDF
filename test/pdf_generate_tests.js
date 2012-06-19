300c5cfca922b52e397389d581b6ebc864c26edb
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-06-18 17:29:15 -0700

    Adding test for addImage

--- test/pdf_generate_tests.js
+++ test/pdf_generate_tests.js
@@ -1,5 +1,56 @@
 $(document).ready(function(){
 
+if (typeof atob === 'undefined') {
+	atob = function (data) {
+    // Decodes string using MIME base64 algorithm  
+    // 
+    // version: 1109.2015
+    // discuss at: http://phpjs.org/functions/base64_decode
+    // +   original by: Tyler Akins (http://rumkin.com)
+    // +   improved by: Thunder.m
+    // +      input by: Aman Gupta
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // +   bugfixed by: Pellentesque Malesuada
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // -    depends on: utf8_decode
+    // *     example 1: base64_decode('S2V2aW4gdmFuIFpvbm5ldmVsZA==');
+    // *     returns 1: 'Kevin van Zonneveld'
+
+	var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var o1, o2, o3, h1, h2, h3, h4, bits, 
+        ac = 0,
+        i = 0,
+        l = data.length
+        tmp_arr = [];
+ 
+    do { // unpack four hexets into three octets using index points in b64
+        h1 = b64.indexOf(data.charAt(i++));
+        h2 = b64.indexOf(data.charAt(i++));
+        h3 = b64.indexOf(data.charAt(i++));
+        h4 = b64.indexOf(data.charAt(i++));
+ 
+        bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+ 
+        o1 = bits >> 16 & 0xff;
+        o2 = bits >> 8 & 0xff;
+        o3 = bits & 0xff;
+ 
+        if (h3 == 64) {
+            tmp_arr[ac++] = String.fromCharCode(o1);
+        } else if (h4 == 64) {
+            tmp_arr[ac++] = String.fromCharCode(o1, o2);
+        } else {
+            tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+        }
+    } while (i < l);
+ 
+    return tmp_arr.join('');
+}
+}
+
 var datestringregex = /\/CreationDate \(D:\d+\)/
 , replacementdatestring = '/CreationDate (D:0)'
 , producerstringregex = /\/Producer\s+\(jsPDF\s+\d+\)/
@@ -211,6 +262,9 @@ var datestringregex = /\/CreationDate \(D:\d+\)/
 	})
 }
 
+////////////////////////////////////////////////////
+// running homogenous tests
+
 for (var filename in testinventory){
 	if (testinventory.hasOwnProperty(filename)){
 		testrunner(
@@ -220,4 +274,42 @@ for (var filename in testinventory){
 	}
 }
 
+// handcrafted tests
+asyncTest('014_addImage', function() {
+
+	//QUnit.stop()
+	require(
+		['text!014_addImage.jpeg.base64.txt', 'text!014_addImage.txt']
+	).then(function(base64encodedJpeg, base64encodedPDF){
+		QUnit.expect(2)
+
+		var pdf = jsPDF()
+
+		pdf.addImage(atob(base64encodedJpeg), 'jpeg', 20, 20)
+
+		QUnit.equal(
+			// just testing if it does not blow up.
+			pdf.output('datauristring') !== ''
+			, true
+		)
+
+		pdf = jsPDF()
+
+		pdf.addImage(atob(base64encodedJpeg), 'jpeg', 20, 20)
+
+		// window.pdfbase64 = btoa(pdf.output())
+
+		QUnit.equal(
+			removeMinorDiffs( pdf.output() )
+			, removeMinorDiffs( atob(base64encodedPDF) )
+		)
+		QUnit.start()
+		//stop()
+	})
+})
+
+
+
 }) // end of document.ready(
+
+
