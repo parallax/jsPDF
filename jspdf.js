a8590ea657ad0adc1f321f9b5e8f75f5d3261613
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-06-20 00:44:00 -0700

    Taking base64encoder declaration outside of jsPDF class. Now patching global btoa()

--- jspdf.js
+++ jspdf.js
@@ -27,6 +27,95 @@
 
 window.jsPDF = (function() {
 
+// patching global with software-based base64 encoder
+// this will run on <=IE9, possibly some niche browsers
+// new webkit-based, FireFox, IE10 already have native version of this.
+if (typeof window === 'object' && !window.btoa) {
+	window.btoa = function(data) {
+		// DO NOT ADD UTF8 ENCODING CODE HERE!!!!
+
+		// UTF8 encoding encodes bytes over char code 128
+		// and, essentially, turns an 8-bit binary streams
+		// (that base64 can deal with) into 7-bit binary streams. 
+		// (by default server does not know that and does not recode the data back to 8bit)
+		// You destroy your data.
+
+		// binary streams like jpeg image data etc, while stored in JavaScript strings,
+		// (which are 16bit arrays) are in 8bit format already.
+		// You do NOT need to char-encode that before base64 encoding.
+
+		// if you, by act of fate
+		// have string which has individual characters with code
+		// above 255 (pure unicode chars), encode that BEFORE you base64 here.
+		// you can use absolutely any approch there, as long as in the end,
+		// base64 gets an 8bit (char codes 0 - 255) stream.
+		// when you get it on the server after un-base64, you must 
+		// UNencode it too, to get back to 16, 32bit or whatever original bin stream.
+
+		// Note, Yes, JavaScript strings are, in most cases UCS-2 - 
+		// 16-bit character arrays. This does not mean, however,
+		// that you always have to UTF8 it before base64.
+		// it means that if you have actual characters anywhere in
+		// that string that have char code above 255, you need to
+		// recode *entire* string from 16-bit (or 32bit) to 8-bit array.
+		// You can do binary split to UTF16 (BE or LE)
+		// you can do utf8, you can split the thing by hand and prepend BOM to it,
+		// but whatever you do, make sure you mirror the opposite on
+		// the server. If server does not expect to post-process un-base64
+		// 8-bit binary stream, think very very hard about messing around with encoding.
+
+		// so, long story short:
+		// DO NOT ADD UTF8 ENCODING CODE HERE!!!!
+		
+		/**
+		====================================================================
+		base64 encoder
+		MIT, GPL
+	
+		version: 1109.2015
+		discuss at: http://phpjs.org/functions/base64_encode
+		+   original by: Tyler Akins (http://rumkin.com)
+		+   improved by: Bayron Guevara
+		+   improved by: Thunder.m
+		+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		+   bugfixed by: Pellentesque Malesuada
+		+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		+   improved by: Rafal Kukawski (http://kukawski.pl)
+		+   			 Daniel Dotsenko, Willow Systems Corp, willow-systems.com
+		====================================================================
+		*/
+		
+		var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+		, b64a = b64.split('')
+		, o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+		ac = 0,
+		enc = "",
+		tmp_arr = [];
+	 
+		do { // pack three octets into four hexets
+			o1 = data.charCodeAt(i++);
+			o2 = data.charCodeAt(i++);
+			o3 = data.charCodeAt(i++);
+	 
+			bits = o1 << 16 | o2 << 8 | o3;
+
+			h1 = bits >> 18 & 0x3f;
+			h2 = bits >> 12 & 0x3f;
+			h3 = bits >> 6 & 0x3f;
+			h4 = bits & 0x3f;
+	 
+			// use hexets to index into b64, and append result to encoded string
+			tmp_arr[ac++] = b64a[h1] + b64a[h2] + b64a[h3] + b64a[h4];
+		} while (i < data.length);
+
+		enc = tmp_arr.join('');
+		var r = data.length % 3;
+		return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+
+		// end of base64 encoder MIT, GPL
+	}
+}
+
 var PubSub = function(context){
 	'use strict'
 	/*  @preserve 
@@ -848,107 +937,20 @@ function jsPDF(/** String */ orientation, /** String */ unit, /** String */ form
 		out(id.toString(10) + ' j')
 	}
 
-	API.base64encode = function(data) {
-		// DO NOT ADD UTF8 ENCODING CODE HERE!!!!
-
-		// UTF8 encoding encodes bytes over char code 128
-		// and, essentially, turns an 8-bit binary streams
-		// (that base64 can deal with) into 7-bit binary streams. 
-		// (by default server does not know that and does not recode the data back to 8bit)
-		// You destroy your data.
-
-		// binary streams like jpeg image data etc, while stored in JavaScript strings,
-		// (which are 16bit arrays) are in 8bit format already.
-		// You do NOT need to char-encode that before base64 encoding.
-
-		// if you, by act of fate
-		// have string which has individual characters with code
-		// above 255 (pure unicode chars), encode that BEFORE you base64 here.
-		// you can use absolutely any approch there, as long as in the end,
-		// base64 gets an 8bit (char codes 0 - 255) stream.
-		// when you get it on the server after un-base64, you must 
-		// UNencode it too, to get back to 16, 32bit or whatever original bin stream.
-
-		// Note, Yes, JavaScript strings are, in most cases UCS-2 - 
-		// 16-bit character arrays. This does not mean, however,
-		// that you always have to UTF8 it before base64.
-		// it means that if you have actual characters anywhere in
-		// that string that have char code above 255, you need to
-		// recode *entire* string from 16-bit (or 32bit) to 8-bit array.
-		// You can do binary split to UTF16 (BE or LE)
-		// you can do utf8, you can split the thing by hand and prepend BOM to it,
-		// but whatever you do, make sure you mirror the opposite on
-		// the server. If server does not expect to post-process un-base64
-		// 8-bit binary stream, think very very hard about messing around with encoding.
-
-		// so, long story short:
-		// DO NOT ADD UTF8 ENCODING CODE HERE!!!!
-
-		// use native code if it's present
-		if (typeof btoa === 'function') return btoa(data)
-		
-		/** @preserve
-		====================================================================
-		base64 encoder
-		MIT, GPL
-	
-		version: 1109.2015
-		discuss at: http://phpjs.org/functions/base64_encode
-		+   original by: Tyler Akins (http://rumkin.com)
-		+   improved by: Bayron Guevara
-		+   improved by: Thunder.m
-		+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-		+   bugfixed by: Pellentesque Malesuada
-		+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-		+   improved by: Rafal Kukawski (http://kukawski.pl)
-		====================================================================
-		*/
-		
-		var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-		, b64a = b64.split('')
-		, o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
-		ac = 0,
-		enc = "",
-		tmp_arr = [];
-	 
-		do { // pack three octets into four hexets
-			o1 = data.charCodeAt(i++);
-			o2 = data.charCodeAt(i++);
-			o3 = data.charCodeAt(i++);
-	 
-			bits = o1 << 16 | o2 << 8 | o3;
-
-			h1 = bits >> 18 & 0x3f;
-			h2 = bits >> 12 & 0x3f;
-			h3 = bits >> 6 & 0x3f;
-			h4 = bits & 0x3f;
-	 
-			// use hexets to index into b64, and append result to encoded string
-			tmp_arr[ac++] = b64a[h1] + b64a[h2] + b64a[h3] + b64a[h4];
-		} while (i < data.length);
-
-		enc = tmp_arr.join('');
-		var r = data.length % 3;
-		return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
-
-		// end of base64 encoder MIT, GPL
-	}
-
 	API.output = function(type, options) {
 		var undef
 		switch (type){
 			case undef: return buildDocument() 
 			case 'datauristring':
 			case 'dataurlstring':
-				return 'data:application/pdf;base64,' + this.base64encode(buildDocument())
+				return 'data:application/pdf;base64,' + window.btoa(buildDocument())
 			case 'datauri':
 			case 'dataurl':
-				document.location.href = 'data:application/pdf;base64,' + this.base64encode(buildDocument()); break;
+				document.location.href = 'data:application/pdf;base64,' + window.btoa(buildDocument()); break;
 			default: throw new Error('Output type "'+type+'" is not supported.') 
 		}
 		// @TODO: Add different output options
 	}
-
 
 	/////////////////////////////////////////
 	// continuing initilisation of jsPDF Document object
