76ccec12f238f046dba0fc94111c206d6c28f323
Lee Driscoll <lsdriscoll@gmail.com>
2013-03-15 10:18:51 +0000

    [DASH-49] Create Export to PDF Added data from store code and printHeaders feature [incomplete]

--- jspdf.plugin.cell.js
+++ jspdf.plugin.cell.js
@@ -49,7 +49,7 @@
 	    return maxLn;
     };
 
-    jsPDFAPI.getTextDimentions = function (txt) {
+    jsPDFAPI.getTextDimensions = function (txt) {
         fontName = this.internal.getFont().fontName;
         fontSize = this.internal.getFontSize();
         fontStyle = this.internal.getFont().fontStyle;
@@ -110,20 +110,155 @@
     doc2.save('Test.pdf');
         
     */
-    
+
+    /**
+     * Create a table from an {#Ext.data.Store} or a set of data.
+     * @param dataSet {Ext.data.Model[]|Ext.data.Store}
+     * @param [headers] {Array} Omit or null to auto-generate headers at a performance cost
+     * @param [autoSize=true] {Boolean}
+     */
+    jsPDFAPI.table = function(dataSet, headers, config){
+
+        var models;
+
+        this.lnMod = 0;
+
+        if(config){
+            var autoSize        = config.autoSize || false,
+                printHeaders    = this.printHeaders = config.printColumnHeaders || true,
+                autoStretch     = config.autoStretch || true;
+        }
+
+        if(!dataSet){
+            Ext.Error.raise('No data for PDF table');
+        }
+
+        if(dataSet.isStore){
+            var isStore = true;
+            models = dataSet.data.items;
+        } else {
+            models = dataSet;
+        }
+
+        // Set headers
+        if(headers === undefined || (headers === null)){
+            // No headers defined so we derive from data
+            if(isStore){
+                headers = Ext.Array.pluck(dataSet.model.getFields(), 'name');
+            } else {
+                headers = Ext.Object.getKeys(models[0]);
+            }
+//            console.log(headers);
+        }
+
+        // Create Columns Matrix
+        var columnMatrix = {},
+            columnWidths = {},
+            index,
+            columnData,
+            TextMetrics = new Ext.util.TextMetrics();
+
+        for (var i = 0, ln = headers.length; i < ln; i++) {
+            index = headers[i];
+            columnMatrix[index] = Ext.Array.map(models, function(rec){
+                debugger;
+                if(!rec.get){
+                    console.log(rec.$className);
+                }
+                return rec.get(index);
+            });
+
+            var columnMinWidths = [], column;
+
+            // get header width
+            columnMinWidths.push(this.getTextDimensions(index).w);
+//            columnMinWidths.push(TextMetrics.getWidth(index)/3.7795275593333);
+
+            column = columnMatrix[index];
+
+            // Get cell widths
+            for (var j = 0, ln = columnMatrix[index].length; j < ln; j++) {
+                columnData = columnMatrix[index][j];
+                columnMinWidths.push(this.getTextDimensions(columnData).w);
+
+//                columnMinWidths.push(TextMetrics.getWidth(columnData)/3.7795275593333);
+            }
+
+            // get final column width
+            columnWidths[index] = Ext.Array.max(columnMinWidths);
+        }
+
+        // Construct the table
+
+        // Construct the header row
+        var header, tableHeaderConfigs = [];
+        for (var i = 0, ln = headers.length; i < ln; i++) {
+            header = headers[i];
+            tableHeaderConfigs.push([10, 10, columnWidths[header], 25, String(header)]);
+        }
+
+        this.setTableHeaderRow(tableHeaderConfigs);
+
+        this.printHeaderRow(1);
+
+        // Construct the data rows
+        var record;
+
+        for (var i = 0, ln = models.length; i < ln; i++) {
+            record = models[i];
+
+            for (var j = 0, jln = headers.length; j < jln; j++) {
+                index = headers[j];
+                this.cell(10, 10, columnWidths[index], 25, String(record.get(index)), i+2);
+            }
+        }
+
+        return this;
+    };
+
+    jsPDFAPI.setTableHeaderRow = function(config){
+        this.tableHeaderRow = config;
+    };
+
+    jsPDFAPI.printHeaderRow = function(lineNumber){
+        this.printingHeaderRow = true;
+        for (var i = 0, ln = this.tableHeaderRow.length; i < ln; i++) {
+            var tableHeaderCell = this.tableHeaderRow[i];
+            var tmp = [].concat(tableHeaderCell);
+            tmp = tmp.concat(lineNumber);
+            this.cell.apply(this,tmp);
+        }
+        this.printingHeaderRow = false;
+    };
+
     jsPDFAPI.cell = function (x, y, w, h, txt, ln) {
+        if(this.printingHeaderRow !== true && this.lnMod !== 0){
+            ln = ln + this.lnMod;
+        }
+
         if ((((ln * h) + y + (h * 2)) / pages) >= this.internal.pageSize.height && pages === 1) {
             this.cellAddPage();
+            if(this.printHeaders && this.tableHeaderRow){
+                this.printHeaderRow(ln);
+                this.lnMod++;
+                ln++;
+            }
             this.setLastCellPosition(undefined, undefined, undefined, undefined, undefined);
             if (this.getMaxLn() === 0) {
                 this.setMaxLn(ln);
             }
         } else if (pages > 1 && this.getMaxLn() === ln / pages) {
             this.cellAddPage();
+            if(this.printHeaders && this.tableHeaderRow){
+                this.printHeaderRow(ln);
+                this.lnMod++;
+                ln++;
+            }
+
             this.setLastCellPosition(undefined, undefined, undefined, undefined, undefined);
         }
-        var curCell = this.getLastCellPosition(),
-            dim = this.getTextDimentions(txt);
+        var curCell = this.getLastCellPosition();
+
         if (curCell.x !== undefined && curCell.ln === ln) {
             x = curCell.x + curCell.w;
             if (curCell.y !== undefined && curCell.y === y) {
