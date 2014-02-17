213c9be65b62b3aead789976ee536126c5e68841
diegocr <dcasorran@gmail.com>
2014-02-17 05:32:45 +0100

    Prettifying jspdf.js as planned in #179, closes #194

--- libs/polyfill.js
+++ libs/polyfill.js
@@ -121,6 +121,21 @@
 		};
 	}
 
+	if (!Array.prototype.forEach) {
+		Array.prototype.forEach = function(fun, thisArg) {
+			"use strict";
+
+			if (this === void 0 || this === null || typeof fun !== "function")
+				throw new TypeError();
+
+			var t = Object(this), len = t.length >>> 0;
+			for (var i = 0; i < len; i++) {
+				if (i in t)
+					fun.call(thisArg, t[i], i, t);
+			}
+		};
+	}
+
 	if (!Object.keys) {
 		Object.keys = (function () {
 			'use strict';
