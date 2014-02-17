61e2214e8ddeba04a4c8a3c24b758d4417bc5539
Mark Chapman <mark.chapman@gmail.com>
2014-02-17 01:24:35 +0000

    Add table footer capability

--- jspdf.plugin.cell.js
+++ jspdf.plugin.cell.js
@@ -1,4 +1,4 @@
-/** ==================================================================== 
+/** ====================================================================
  * jsPDF Cell plugin
  * Copyright (c) 2013 Youssef Beddad, youssef.beddad@gmail.com
  *               2013 Eduardo Menezes de Morais, eduardo.morais@usp.br
@@ -13,10 +13,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -119,12 +119,16 @@
                 this.rect(x, y, w, h);
             }
             if (align === 'right') {
+                var textSize;
                 if (txt instanceof Array) {
                     for(var i = 0; i<txt.length; i++) {
                         var currentLine = txt[i];
-                        var textSize = this.getStringUnitWidth(currentLine) * this.internal.getFontSize();
+                        textSize = this.getStringUnitWidth(currentLine) * this.internal.getFontSize() / (72/25.6);
                         this.text(currentLine, x + w - textSize - padding, y + this.internal.getLineHeight()*(i+1));
                     }
+                } else {
+                    textSize = this.getStringUnitWidth(txt) * this.internal.getFontSize() / (72/25.6);
+                    this.text(txt, x + w - textSize - padding, y + this.internal.getLineHeight());
                 }
             } else {
                 this.text(txt, x + padding, y + this.internal.getLineHeight());
@@ -141,23 +145,23 @@
      */
     jsPDFAPI.getKeys = (typeof Object.keys === 'function')
         ? function (object) {
-            if (!object) {
-                return [];
-            }
-            return Object.keys(object);
+        if (!object) {
+            return [];
         }
-            : function (object) {
-            var keys = [],
-                property;
+        return Object.keys(object);
+    }
+        : function (object) {
+        var keys = [],
+            property;
 
-            for (property in object) {
-                if (object.hasOwnProperty(property)) {
-                    keys.push(property);
-                }
+        for (property in object) {
+            if (object.hasOwnProperty(property)) {
+                keys.push(property);
             }
+        }
 
-            return keys;
-        };
+        return keys;
+    };
 
     /**
      * Return the maximum value from an array
@@ -194,14 +198,14 @@
      * @param {Integer} [y] top-position for top-left corner of table
      * @param {Object[]} [data] As array of objects containing key-value pairs corresponding to a row of data.
      * @param {String[]} [headers] Omit or null to auto-generate headers at a performance cost
-
+     * @param {Object} footers Object containing key-value pairs.  Omit or null if not required
      * @param {Object} [config.printHeaders] True to print column headers at the top of every page
      * @param {Object} [config.autoSize] True to dynamically set the column widths to match the widest cell value
      * @param {Object} [config.margins] margin values for left, top, bottom, and width
      * @param {Object} [config.fontSize] Integer fontSize to use (optional)
      */
 
-    jsPDFAPI.table = function (x,y, data, headers, config) {
+    jsPDFAPI.table = function (x,y, data, headers, footers, config) {
         if (!data) {
             throw 'No data for PDF table';
         }
@@ -217,6 +221,7 @@
             columnData,
             column,
             columnMinWidths = [],
+            columnAligns = [],
             j,
             tableHeaderConfigs = [],
             model,
@@ -224,13 +229,13 @@
             func,
 
         //set up defaults. If a value is provided in config, defaults will be overwritten:
-           autoSize        = false,
-           printHeaders    = true,
-           fontSize        = 12,
-           margins         = {left:0, top:0, bottom: 0, width: this.internal.pageSize.width};
+            autoSize        = false,
+            printHeaders    = true,
+            fontSize        = 12,
+            margins         = {left:0, top:0, bottom: 0, width: this.internal.pageSize.width};
 
         if (config) {
-        //override config defaults if the user has specified non-default behavior:
+            //override config defaults if the user has specified non-default behavior:
             if(config.autoSize === true) {
                 autoSize = true;
             }
@@ -251,7 +256,7 @@
          */
         this.lnMod = 0;
         lastCellPos = { x: undefined, y: undefined, w: undefined, h: undefined, ln: undefined },
-        pages = 1;
+            pages = 1;
 
         this.printHeaders = printHeaders;
         this.margins = margins;
@@ -272,12 +277,12 @@
                 headerNames.push(header.name);
                 headerPrompts.push(header.prompt);
                 columnWidths[header.name] = header.width *px2pt;
+                columnAligns[header.name] = header.align;
             }
 
         } else {
             headerNames = headers;
         }
-
         if (autoSize) {
             // Create a matrix of columns e.g., {column_title: [row1_Record, row2_Record]}
             func = function (rec) {
@@ -301,6 +306,11 @@
                     columnMinWidths.push(this.getTextDimensions(columnData).w);
                 }
 
+                // get footer width
+                if (footers) {
+                    columnMinWidths.push(this.getTextDimensions(footers[i]).w);
+                }
+
                 // get final column width
                 columnWidths[header] = jsPDFAPI.arrayMax(columnMinWidths);
             }
@@ -314,7 +324,7 @@
             // Construct the header row
             for (i = 0, ln = headerNames.length; i < ln; i += 1) {
                 header = headerNames[i];
-                tableHeaderConfigs.push([x, y, columnWidths[header], lineHeight, String(headerPrompts.length ? headerPrompts[i] : header)]);
+                tableHeaderConfigs.push([x, y, columnWidths[header], lineHeight, String(headerPrompts.length ? headerPrompts[i] : header),0,columnAligns[header]]);
             }
 
             // Store the table header config
@@ -332,10 +342,20 @@
 
             for (j = 0, jln = headerNames.length; j < jln; j += 1) {
                 header = headerNames[j];
-                this.cell(x, y, columnWidths[header], lineHeight, model[header], i + 2, header.align);
+                this.cell(x, y, columnWidths[header], lineHeight, model[header], i + 2, columnAligns[header]);
             }
         }
-        this.lastCellPos = lastCellPos;
+
+        if (footers) {
+            // Construct the header row
+            for (var fi = 0; fi < headerNames.length; fi ++) {
+                header = headerNames[fi];
+                tableHeaderConfigs[fi][4] = footers[header] || ' ';
+            }
+
+            // Print the header for the start of the table
+            this.printHeaderRow(i + 2, false);
+        }
         this.table_x = x;
         this.table_y = y;
         return this;
@@ -398,7 +418,8 @@
                 tempHeaderConf.push(tableHeaderCell);
             }
             tmpArray = [].concat(tableHeaderCell);
-            this.cell.apply(this, tmpArray.concat(lineNumber));
+            tmpArray[5] = lineNumber;
+            this.cell.apply(this, tmpArray);
         }
         if (tempHeaderConf.length > 0){
             this.setTableHeaderRow(tempHeaderConf);
