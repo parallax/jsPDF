bd9fd9867b504dbcd6b475052cde5aadddd6e3ab
Wolfgang Gassler <wolfgang@gassler.org>
2014-04-29 20:55:15 +0200

    processing of css style text-align center and right added

--- jspdf.plugin.from_html.js
+++ jspdf.plugin.from_html.js
@@ -26,7 +26,11 @@
  */
 
 (function(jsPDFAPI) {
-  var DrillForContent, FontNameDB, FontStyleMap, FontWeightMap, GetCSS, PurgeWhiteSpace, Renderer, ResolveFont, ResolveUnitedNumber, UnitedNumberMap, elementHandledElsewhere, images, loadImgs, process, tableToJson;
+  var clone,DrillForContent, FontNameDB, FontStyleMap, FontWeightMap, GetCSS, PurgeWhiteSpace, Renderer, ResolveFont, ResolveUnitedNumber, UnitedNumberMap, elementHandledElsewhere, images, loadImgs, process, tableToJson;
+  clone = (function(){ 
+    return function (obj) { Clone.prototype=obj; return new Clone() };
+    function Clone(){}
+  }());
   PurgeWhiteSpace = function(array) {
     var fragment, i, l, lTrimmed, r, rTrimmed, trailingSpace;
     i = 0;
@@ -140,6 +144,7 @@
     tmp = void 0;
     css["font-family"] = ResolveFont(computedCSSElement("font-family")) || "times";
     css["font-style"] = FontStyleMap[computedCSSElement("font-style")] || "normal";
+	css["text-align"] = TextAlignMap[computedCSSElement("text-align")] || "left";
     tmp = FontWeightMap[computedCSSElement("font-weight")] || "normal";
     if (tmp === "bold") {
       if (css["font-style"] === "normal") {
@@ -399,8 +404,28 @@
         }
       }
     }
+	
+	//if text alignment was set, set margin/indent of each line
+	if (style['text-align'] !== undefined && (style['text-align'] === 'center' || style['text-align'] === 'right')) {
+		for(var i = 0; i < lines.length; ++i) {
+			var length = this.pdf.getStringUnitWidth(lines[i][0][0], fragmentSpecificMetrics) * fragmentSpecificMetrics.fontSize / k;
+			//if there is more than on line we have to clone the style object as all lines hold a reference on this object
+			if (i > 0) {
+				lines[i][0][1] = clone(lines[i][0][1]);
+			}
+			var space = (maxLineLength-length);
+			console.log(lines[i][0][0]+" with space: "+space)
+			if (style['text-align'] === 'right') {
+				lines[i][0][1]['margin-left'] = space;
+			//if alignment is not right, it has to be center so split the space to the left and the right
+			} else {
+				lines[i][0][1]['margin-left'] = space/2;
+			}
+		};
+	}
+
     return lines;
-  };
+  }; 
   Renderer.prototype.RenderTextFragment = function(text, style) {
     var defaultFontSize, font;
     if (this.pdf.internal.pageSize.height - this.pdf.margins_doc.bottom < this.y + this.pdf.internal.getFontSize()) {
@@ -411,7 +436,7 @@
     }
     defaultFontSize = 12;
     font = this.pdf.internal.getFont(style["font-family"], style["font-style"]);
-    return this.pdf.internal.write("/" + font.id, (defaultFontSize * style["font-size"]).toFixed(2), "Tf", "(" + this.pdf.internal.pdfEscape(text) + ") Tj");
+	return this.pdf.internal.write("/" + font.id, (defaultFontSize * style["font-size"]).toFixed(2), "Tf", "(" + this.pdf.internal.pdfEscape(text) + ") Tj");;
   };
   Renderer.prototype.renderParagraph = function() {
     var blockstyle, defaultFontSize, fontToUnitRatio, fragments, i, l, line, lines, maxLineHeight, out, paragraphspacing_after, paragraphspacing_before, priorblockstype, styles;
@@ -440,7 +465,11 @@
     l = void 0;
     this.y += paragraphspacing_before;
     out("q", "BT", this.pdf.internal.getCoordinateString(this.x), this.pdf.internal.getVerticalCoordinateString(this.y), "Td");
-    while (lines.length) {
+	
+	//stores the current indent of cursor position
+	var currentIndent = 0;
+    
+	while (lines.length) {
       line = lines.shift();
       maxLineHeight = 0;
       i = 0;
@@ -451,7 +480,16 @@
         }
         i++;
       }
-      out(0, (-1 * defaultFontSize * maxLineHeight).toFixed(2), "Td");
+	  //if we have to move the cursor to adapt the indent
+	  var indentMove = 0;
+	  //if a margin was added (by e.g. a text-alignment), move the cursor
+	  if (line[0][1]["margin-left"] !== undefined && line[0][1]["margin-left"] > 0) {
+		wantedIndent = this.pdf.internal.getCoordinateString(line[0][1]["margin-left"]);
+		indentMove = wantedIndent-currentIndent;
+		currentIndent = wantedIndent;
+	  } 	  
+	  //move the cursor
+      out(indentMove, (-1 * defaultFontSize * maxLineHeight).toFixed(2), "Td");
       i = 0;
       l = line.length;
       while (i !== l) {
@@ -503,6 +541,11 @@
     normal: "normal",
     italic: "italic",
     oblique: "italic"
+  };
+  TextAlignMap = {
+	left: "left",
+    right: "right",
+    center: "center"
   };
   UnitedNumberMap = {
     normal: 1
