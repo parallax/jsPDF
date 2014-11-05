04a0aa6dd35610902f47997bb8f3386422dc95eb
Luís Fernando Vendrame <lvendrame@gmail.com>
2014-11-05 16:17:32 -0200

    Update jspdf.plugin.cell.js
    Fix new page bug. When new page is added, the var "y" is set to NaN.

--- jspdf.plugin.cell.js
+++ jspdf.plugin.cell.js
@@ -110,9 +110,10 @@
                     if (this.printHeaders && this.tableHeaderRow) {
                         this.printHeaderRow(ln, true);
                     }
+                }else{
+                    //We ignore the passed y: the lines may have diferent heights
+                    y = (getLastCellPosition().y + getLastCellPosition().h);
                 }
-                //We ignore the passed y: the lines may have diferent heights
-                y = (getLastCellPosition().y + getLastCellPosition().h);
 
             }
         }
