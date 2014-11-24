8fd15497657da82bc6525425a08a2ca3cfe2f0e4
Kenneth Glassey <awereal@gmail.com>
2014-11-23 19:38:53 -0800

    Refactored API.text() for text alignment support closes #383

--- jspdf.js
+++ jspdf.js
@@ -1111,6 +1111,22 @@ var jsPDF = (function(global) {
 			if (typeof flags === 'string') {
 				align = flags;
 				flags = null;
+			// Convert text into an array anyway to simplify
+			// later code.
+			if (typeof text === 'string') {
+				if(text.match(/[\n\r]/)) {
+					text = text.split( /\r\n|\r|\n/g);
+				} else {
+					text = [text];
+				}			
+			}
+			if (typeof angle === 'string') {
+				align = angle;
+				angle = null;
+			}
+			if (typeof flags === 'string') {
+				align = flags;
+				flags = null;
 			}
 			if (typeof flags === 'number') {
 				angle = flags;
@@ -1145,7 +1161,9 @@ var jsPDF = (function(global) {
 				pageContext.lastTextWasStroke = false;
 			}
 			
-			if (text instanceof Array) {
+			if (typeof text === 'string') {
+				text = ESC(text);
+			} else if (text instanceof Array) {
 				// we don't want to destroy  original text array, so cloning it
 				var sa = text.concat(), da = [], i, len = sa.length;
 				// we do array.join('text that must not be PDFescaped")
@@ -1187,6 +1205,47 @@ var jsPDF = (function(global) {
 						// PDF Spec 1.3 p.288
 						text += ") Tj\n" + delta + " -" + leading + " Td (" + da[i];
 						prevX += delta;
+					}			
+				} else {
+					text = da.join(") Tj\nT* (");
+				}
+				
+				if( align ) {					
+					var left,
+						prevX,
+						maxLineLength,
+						leading =  activeFontSize * lineHeightProportion,
+						lineWidths = text.map( function( v ) { 
+							return this.getStringUnitWidth( v ) * activeFontSize / k;
+						}, this );
+					maxLineLength = Math.max.apply( Math, lineWidths );
+					// The first line uses the "main" Td setting,
+					// and the subsequent lines are offset by the
+					// previous line's x coordinate.
+					if( align === "center" ) {
+						// The passed in x coordinate defines
+						// the center point.
+						left = x - maxLineLength / 2;							
+						x -= lineWidths[0] / 2;
+					} else if ( align === "right" ) {
+						// The passed in x coordinate defines the
+						// rightmost point of the text.
+						left = x - maxLineLength;							
+						x -= lineWidths[0];
+					} else {
+						throw new Error('Unrecognized alignment option, use "center" or "right".');
+					}
+					prevX = x;
+					text = da[0] + ") Tj\n";
+					for ( i = 1, len = da.length ; i < len; i++ ) {
+						var delta = maxLineLength - lineWidths[i];
+						if( align === "center" ) delta /= 2;
+						// T* = x-offset leading Td ( text )
+						text += ( ( left - prevX ) + delta ) + " -" + leading + " Td (" + da[i];
+						prevX = left + delta;
+						if( i < len - 1 ) {
+							text += ") Tj\n";
+						}
 					}			
 				} else {
 					text = da.join(") Tj\nT* (");
