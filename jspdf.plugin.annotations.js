83719661fa5bf5fcf00344752cf558296e401196
Steven Spungin <steven@spungin.tv>
2015-02-05 09:17:18 -0500

    Fixed a bug that caused reader to crash

--- jspdf.plugin.annotations.js
+++ jspdf.plugin.annotations.js
@@ -74,136 +74,136 @@
 
 	jsPDF.API.annotationPlugin = annotationPlugin;
 
-	jsPDF.API.events.push([
-			'addPage', function(info) {
-				this.annotationPlugin.annotations[info.pageNumber] = [];
+	jsPDF.API.events.push([ 'addPage', function(info) {
+		this.annotationPlugin.annotations[info.pageNumber] = [];
+	} ]);
+
+	jsPDFAPI.events.push([ 'putPage', function(info) {
+		//TODO store annotations in pageContext so reorder/remove will not affect them.
+		var pageAnnos = this.annotationPlugin.annotations[info.pageNumber];
+
+		var found = false;
+		for (var a = 0; a < pageAnnos.length && !found; a++) {
+			var anno = pageAnnos[a];
+			switch (anno.type) {
+			case 'link':
+				if (annotationPlugin.notEmpty(anno.options.url) || annotationPlugin.notEmpty(anno.options.pageNumber)) {
+					found = true;
+					break;
+				}
+			case 'text':
+			case 'freetext':
+				found = true;
+				break;
 			}
-	]);
+		}
+		if (found == false) {
+			return;
+		}
 
-	jsPDFAPI.events.push([
-			'putPage', function(info) {
-				//TODO store annotations in pageContext so reorder/remove will not affect them.
-				var pageAnnos = this.annotationPlugin.annotations[info.pageNumber];
+		this.internal.write("/Annots [");
+		var f2 = this.annotationPlugin.f2;
+		var k = this.internal.scaleFactor;
+		var pageHeight = this.internal.pageSize.height;
+		var pageInfo = this.internal.getPageInfo(info.pageNumber);
+		for (var a = 0; a < pageAnnos.length; a++) {
+			var anno = pageAnnos[a];
 
-				var found = false;
-				for (var a = 0; a < pageAnnos.length && !found; a++) {
-					var anno = pageAnnos[a];
-					switch (anno.type) {
-					case 'link':
-						if (annotationPlugin.notEmpty(anno.options.url) || annotationPlugin.notEmpty(anno.options.pageNumber)) {
-							found = true;
-							break;
-						}
-					case 'text':
-					case 'freetext':
-						found = true;
-						break;
+			switch (anno.type) {
+			case 'text':
+				// Create a an object for both the text and the popup
+				var objText = this.internal.newAdditionalObject();
+				var objPopup = this.internal.newAdditionalObject();
+
+				var title = anno.title || 'Note';
+				var rect = "/Rect [" + f2(anno.bounds.x * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + " " + f2((anno.bounds.x + anno.bounds.w) * k) + " " + f2((pageHeight - anno.bounds.y) * k) + "] ";
+				line = '<</Type /Annot /Subtype /' + 'Text' + ' ' + rect + '/Contents (' + anno.contents + ')';
+				line += ' /Popup ' + objPopup.objId + " 0 R";
+				line += ' /P ' + pageInfo.objId + " 0 R";
+				line += ' /T (' + title + ') >>';
+				objText.content = line;
+
+				var parent = objText.objId + ' 0 R';
+				var popoff = 30;
+				var rect = "/Rect [" + f2((anno.bounds.x + popoff) * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + " " + f2((anno.bounds.x + anno.bounds.w + popoff) * k) + " " + f2((pageHeight - anno.bounds.y) * k) + "] ";
+				//var rect2 = "/Rect [" + f2(anno.bounds.x * k) + " " + f2((pageHeight - anno.bounds.y) * k) + " " + f2(anno.bounds.x + anno.bounds.w * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + "] ";
+				line = '<</Type /Annot /Subtype /' + 'Popup' + ' ' + rect + ' /Parent ' + parent;
+				if (anno.open) {
+					line += ' /Open true';
+				}
+				line += ' >>';
+				objPopup.content = line;
+
+				this.internal.write(objText.objId, '0 R', objPopup.objId, '0 R');
+
+				break;
+			case 'freetext':
+				var rect = "/Rect [" + f2(anno.bounds.x * k) + " " + f2((pageHeight - anno.bounds.y) * k) + " " + f2(anno.bounds.x + anno.bounds.w * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + "] ";
+				var color = anno.color || '#000000';
+				line = '<</Type /Annot /Subtype /' + 'FreeText' + ' ' + rect + '/Contents (' + anno.contents + ')';
+				line += ' /DS(font: Helvetica,sans-serif 12.0pt; text-align:left; color:#' + color + ')';
+				line += ' /Border [0 0 0]';
+				line += ' >>';
+				this.internal.write(line);
+				break;
+			case 'link':
+				if (anno.options.name) {
+					var loc = this.annotations._nameMap[anno.options.name];
+					anno.options.pageNumber = loc.page;
+					anno.options.top = loc.y;
+				} else {
+					if (!anno.options.top) {
+						anno.options.top = 0;
 					}
 				}
-				if (found == false) {
-					return;
-				}
 
-				this.internal.write("/Annots [");
-				var f2 = this.annotationPlugin.f2;
-				var k = this.internal.scaleFactor;
-				var pageHeight = this.internal.pageSize.height;
-				var pageInfo = this.internal.getPageInfo(info.pageNumber);
-				for (var a = 0; a < pageAnnos.length; a++) {
-					var anno = pageAnnos[a];
+				//var pageHeight = this.internal.pageSize.height * this.internal.scaleFactor;
+				var rect = "/Rect [" + f2(anno.x * k) + " " + f2((pageHeight - anno.y) * k) + " " + f2(anno.x + anno.w * k) + " " + f2(pageHeight - (anno.y + anno.h) * k) + "] ";
 
-					switch (anno.type) {
-					case 'text':
-						// Create a an object for both the text and the popup
-						var objText = this.internal.newAdditionalObject();
-						var objPopup = this.internal.newAdditionalObject();
-
-						var title = anno.title || 'Note';
-						var rect = "/Rect [" + f2(anno.bounds.x * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + " " + f2((anno.bounds.x + anno.bounds.w) * k) + " " + f2((pageHeight - anno.bounds.y) * k) + "] ";
-						line = '<</Type /Annot /Subtype /' + 'Text' + ' ' + rect + '/Contents (' + anno.contents + ')';
-						line += ' /Popup ' + objPopup.objId + " 0 R";
-						line += ' /P ' + pageInfo.objId + " 0 R";
-						line += ' /T (' + title + ') >>';
-						objText.content = line;
-
-						var parent = objText.objId + ' 0 R';
-						var popoff = 30;
-						var rect = "/Rect [" + f2((anno.bounds.x + popoff) * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + " " + f2((anno.bounds.x + anno.bounds.w + popoff) * k) + " " + f2((pageHeight - anno.bounds.y) * k) + "] ";
-						//var rect2 = "/Rect [" + f2(anno.bounds.x * k) + " " + f2((pageHeight - anno.bounds.y) * k) + " " + f2(anno.bounds.x + anno.bounds.w * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + "] ";
-						line = '<</Type /Annot /Subtype /' + 'Popup' + ' ' + rect + ' /Parent ' + parent;
-						if (anno.open) {
-							line += ' /Open true';
-						}
-						line += ' >>';
-						objPopup.content = line;
-
-						this.internal.write(objText.objId, '0 R', objPopup.objId, '0 R');
-
+				var line = '';
+				if (anno.options.url) {
+					line = '<</Type /Annot /Subtype /Link ' + rect + '/Border [0 0 0] /A <</S /URI /URI (' + anno.options.url + ') >>';
+				} else if (anno.options.pageNumber) {
+					// first page is 0
+					var info = this.internal.getPageInfo(anno.options.pageNumber);
+					line = '<</Type /Annot /Subtype /Link ' + rect + '/Border [0 0 0] /Dest [' + info.objId + " 0 R";
+					anno.options.magFactor = anno.options.magFactor || "XYZ";
+					switch (anno.options.magFactor) {
+					case 'Fit':
+						line += ' /Fit]';
 						break;
-					case 'freetext':
-						var rect = "/Rect [" + f2(anno.bounds.x * k) + " " + f2((pageHeight - anno.bounds.y) * k) + " " + f2(anno.bounds.x + anno.bounds.w * k) + " " + f2(pageHeight - (anno.bounds.y + anno.bounds.h) * k) + "] ";
-						var color = anno.color || '#000000';
-						line = '<</Type /Annot /Subtype /' + 'FreeText' + ' ' + rect + '/Contents (' + anno.contents + ')';
-						line += ' /DS(font: Helvetica,sans-serif 12.0pt; text-align:left; color:#' + color + ')';
-						line += ' /Border [0 0 0]';
-						line += ' >>';
-						this.internal.write(line);
+					case 'FitH':
+						//anno.options.top = anno.options.top || f2(pageHeight * k);
+						line += ' /FitH ' + anno.options.top + ']';
 						break;
-					case 'link':
-						if (anno.options.name) {
-							var loc = this.annotations._nameMap[anno.options.name];
-							anno.options.pageNumber = loc.page;
-							anno.options.top = loc.y;
+					case 'FitV':
+						anno.options.left = anno.options.left || 0;
+						line += ' /FitV ' + anno.options.left + ']';
+						break;
+					case 'XYZ':
+					default:
+						var top = f2((pageHeight - anno.options.top) * k);// || f2(pageHeight * k);
+						anno.options.left = anno.options.left || 0;
+						// 0 or null zoom will not change zoom factor
+						if (typeof anno.options.zoom === 'undefined') {
+							anno.options.zoom = 0;
 						}
-
-						//var pageHeight = this.internal.pageSize.height * this.internal.scaleFactor;
-						var rect = "/Rect [" + f2(anno.x * k) + " " + f2((pageHeight - anno.y) * k) + " " + f2(anno.x + anno.w * k) + " " + f2(pageHeight - (anno.y + anno.h) * k) + "] ";
-
-						var line = '';
-						if (anno.options.url) {
-							line = '<</Type /Annot /Subtype /Link ' + rect + '/Border [0 0 0] /A <</S /URI /URI (' + anno.options.url + ') >>';
-						} else if (anno.options.pageNumber) {
-							// first page is 0
-							var info = this.internal.getPageInfo(anno.options.pageNumber);
-							line = '<</Type /Annot /Subtype /Link ' + rect + '/Border [0 0 0] /Dest [' + info.objId + " 0 R";
-							anno.options.magFactor = anno.options.magFactor || "XYZ";
-							switch (anno.options.magFactor) {
-							case 'Fit':
-								line += ' /Fit]';
-								break;
-							case 'FitH':
-								anno.options.top = anno.options.top || f2(pageHeight * k);
-								line += ' /FitH ' + anno.options.top + ']';
-								break;
-							case 'FitV':
-								anno.options.left = anno.options.left || 0;
-								line += ' /FitV ' + anno.options.left + ']';
-								break;
-							case 'XYZ':
-							default:
-								var top = f2((pageHeight - anno.options.top) * k) || f2(pageHeight * k);
-								anno.options.left = anno.options.left || 0;
-								// 0 or null zoom will not change zoom factor
-								if (typeof anno.options.zoom === 'undefined') {
-									anno.options.zoom = 0;
-								}
-								line += ' /XYZ ' + anno.options.left + ' ' + top + ' ' + anno.options.zoom + ']';
-								break;
-							}
-						} else {
-							// TODO error - should not be here
-						}
-						if (line != '') {
-							line += " >>";
-							this.internal.write(line);
-						}
+						line += ' /XYZ ' + anno.options.left + ' ' + top + ' ' + anno.options.zoom + ']';
 						break;
 					}
-
+				} else {
+					// TODO error - should not be here
 				}
-				this.internal.write("]");
+				if (line != '') {
+					line += " >>";
+					this.internal.write(line);
+				}
+				break;
 			}
-	]);
+
+		}
+		this.internal.write("]");
+	} ]);
 
 	jsPDFAPI.createAnnotation = function(options) {
 		switch (options.type) {
