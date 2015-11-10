91cdbed13251ab71a9d076babfdd7528ca19a422
Vlad Tsepelev <reg.yeti@gmail.com>
2015-11-10 18:44:00 +0800

    add fast decoding by using TextDecoder where available

--- plugins/addimage.js
+++ plugins/addimage.js
@@ -413,6 +413,11 @@
 	 * Async method using Blob and FileReader could be best, but i'm not sure how to fit it into the flow?
 	 */
 	jsPDFAPI.arrayBufferToBinaryString = function(buffer) {
+		if('TextDecoder' in window){
+			var decoder = new TextDecoder('ascii');
+			return decoder.decode(buffer);
+		}
+
 		if(this.isArrayBuffer(buffer))
 			buffer = new Uint8Array(buffer);
 
