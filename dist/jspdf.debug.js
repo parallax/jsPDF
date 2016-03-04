76286898abd34401cb923f8bcdeebf0f2bee2ad5
Lukas Hollaender <lukas.hollaender@yworks.com>
2016-03-04 10:14:04 +0100

    Build

--- dist/jspdf.debug.js
+++ dist/jspdf.debug.js
@@ -1,7 +1,7 @@
 /** @preserve
  * jsPDF - PDF Document creation from JavaScript
- * Version 1.2.21-git Built on 2016-01-21T10:15
- *                           CommitID da45c161fd
+ * Version 1.2.64-git Built on 2016-03-04T10:06
+ *                           CommitID 93ff08cc3c
  *
  * Copyright (c) 2010-2014 James Hall <james@parall.ax>, https://github.com/MrRio/jsPDF
  *               2010 Aaron Spike, https://github.com/acspike
@@ -2831,7 +2831,7 @@ var jsPDF = (function(global) {
 	 * pdfdoc.mymethod() // <- !!!!!!
 	 */
 	jsPDF.API = {events:[]};
-	jsPDF.version = "1.2.21-debug 2016-01-21T10:15:hollaender";
+	jsPDF.version = "1.2.64-debug 2016-03-04T10:06:hollaender";
 
 	if (typeof define === 'function' && define.amd) {
 		define('jsPDF', function() {
@@ -5262,6 +5262,7 @@ var jsPDF = (function(global) {
 	DrillForContent,
 	FontNameDB,
 	FontStyleMap,
+	TextAlignMap,
 	FontWeightMap,
 	FloatMap,
 	ClearMap,
