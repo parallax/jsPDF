804785384e04e6aed9e9162de3a88bf8bc4ada01
Simon Bengtsson <simonbengt@gmail.com>
2016-03-23 00:07:16 +0100

    Use rollupjs for bundling jspdf, plugins and libs

--- build.js
+++ build.js
@@ -1,55 +1,80 @@
 var fs = require('fs');
+var rollup = require('rollup');
 var uglify = require('uglify-js');
 var execSync = require('child_process').execSync;
 
-var paths = {
-    adler: 'node_modules/adler32cs/adler32cs.js',
-    fileSaver: 'node_modules/filesaver.js/FileSaver.js',
-    adlerTmp: 'adler.tmp.js',
-    fileSaverTmp: 'filesaver.tmp.js',
+bundle({
     minified: 'dist/jspdf.min.js',
     debug: 'dist/jspdf.debug.js'
-};
-
-var libs = [
-    'node_modules/cf-blob.js/Blob.js',
-    paths.fileSaverTmp,
-    paths.adlerTmp,
-    'libs/css_colors.js',
-    'libs/deflate.js',
-    'libs/html2canvas/dist/html2canvas.js',
-    'libs/png_support/png.js',
-    'libs/png_support/zlib.js',
-    'libs/polyfill.js'
-];
-var files = execSync('find plugins/*.js').toString().trim().split(/\n/);
-files.unshift('jspdf.js');
-
-monkeyPatch();
-bundle();
-
-fs.unlink(paths.adlerTmp);
-fs.unlink(paths.fileSaverTmp);
+});
 
 // Monkey patching adler32 and filesaver
 function monkeyPatch() {
-    var adler = fs.readFileSync(paths.adler).toString();
-    adler = adler.replace(/this, function/g, 'jsPDF, function');
-    adler = adler.replace(/typeof define/g, '0');
-    fs.writeFileSync(paths.adlerTmp, adler);
-
-    var fileSaverCode = fs.readFileSync(paths.fileSaver).toString();
-    fileSaverCode = fileSaverCode.replace(/define !== null\) && \(define.amd != null/g, '0');
-    fs.writeFileSync(paths.fileSaverTmp, fileSaverCode);
+    return {
+        transform: function (code, id) {
+            var file = id.split('/').pop();
+            if (file === 'adler32cs.js') {
+                code = code.replace(/this, function/g, 'jsPDF, function');
+                code = code.replace(/typeof define/g, '0');
+            } else if (file === 'FileSaver.js') {
+                code = code.replace(/define !== null\) && \(define.amd != null/g, '0');
+            }
+            return code;
+        }
+    }
 }
 
-function bundle() {
-    var debug = execSync('cat ' + files.join(' ') + ' ' + libs.join(' ')).toString().trim();
-    fs.writeFileSync(paths.debug, renew(debug));
+// Rollup removes local variables unless used within a module.
+// This plugin makes sure specified local variables are preserved 
+// and kept local. This plugin won't be necessary once es2015
+// modules are used.
+function rawjs(opts) {
+    opts = opts || {};
+    return {
+        transform: function (code, id) {
+            var variable = opts[id.split('/').pop()];
+            if (!variable) return code;
 
-    var licences = extractLicences();
-    var minifed = uglify.minify(files.concat(libs), {wrap: 'jspdf-exports'});
-    fs.writeFileSync(paths.minified, renew('' + licences + minifed.code));
+            var keepStr = '/*rollup-keeper-start*/window.tmp=' + variable + ';/*rollup-keeper-end*/';
+            return code + keepStr;
+        },
+        transformBundle: function (code) {
+            for (var file in opts) {
+                var r = new RegExp(opts[file] + '\\$\\d+', 'g');
+                code = code.replace(r, opts[file]);
+            }
+            var re = /\/\*rollup-keeper-start\*\/.*\/\*rollup-keeper-end\*\//g;
+            return code.replace(re, '');
+        }
+    }
+}
+
+function bundle(paths) {
+    rollup.rollup({
+        entry: './libs/main.js',
+        plugins: [
+            monkeyPatch(),
+            rawjs({
+                'jspdf.js': 'jsPDF',
+                'filesaver.tmp.js': 'saveAs',
+                'deflate.js': 'Deflater',
+                'zlib.js': 'FlateStream',
+                'css_colors.js': 'CssColors',
+                'html2pdf.js': 'html2pdf'
+            })
+        ]
+    }).then(function (bundle) {
+        var code = bundle.generate({format: 'umd'}).code;
+        code = code.replace(/'use strict';/, '');
+        code = code.replace(/Permission\s+is\s+hereby\s+granted[\S\s]+?IN\s+THE\s+SOFTWARE\./, 'Licensed under the MIT License');
+        code = code.replace(/Permission\s+is\s+hereby\s+granted[\S\s]+?IN\s+THE\s+SOFTWARE\./g, '');
+        fs.writeFileSync(paths.debug, renew(code));
+
+        var minified = uglify.minify(code, {fromString: true, output: {comments: /@preserve|@license|copyright/i}});
+        fs.writeFileSync(paths.minified, renew(minified.code));
+    }).catch(function (err) {
+        console.error(err);
+    });
 }
 
 function renew(code) {
@@ -63,20 +88,4 @@ function renew(code) {
     code = code.replace(/1\.0\.0-trunk/, version + ' ' + date + ':' + whoami);
 
     return code;
-}
-
-function extractLicences() {
-    var licences = '';
-
-    files.concat(libs).forEach(function(file) {
-        var commentFilter = " | sed -n -e '1,/\\*\\//p' | sed -e 's/\\s*@preserve/ /' | grep -v *global";
-        if (file !== 'jspdf.js') {
-            commentFilter += " | sed -e '/Permission/,/SOFTWARE\\./c \\'";
-        }
-
-        var lic = execSync("awk '/^\\/\\*/,/\\*\\//' " + file + commentFilter).toString();
-        licences += lic.replace(/^\s*\n/gm, '');
-    });
-
-    return licences;
 }
