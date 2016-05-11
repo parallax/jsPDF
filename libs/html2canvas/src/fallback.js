0b0216b324783dca3ed8ae93689b2c3d449f117b
thespooler <spooler@venrik.com>
2016-05-11 15:51:42 -0400

    Removing files to be replaced by submodules

--- libs/html2canvas/src/fallback.js
+++
@@ -1,6 +0,0 @@
-if (typeof(Object.create) !== "function" || typeof(document.createElement("canvas").getContext) !== "function") {
-    window.html2canvas = function() {
-        return Promise.reject("No canvas support");
-    };
-    //return;
-}
