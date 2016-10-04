ce31176363664d1563fbbe7225704040de89e26a
James Hall <james@parall.ax>
2016-10-04 14:48:26 +0100

    Update style

--- specs/text/standard.spec.js
+++ specs/text/standard.spec.js
@@ -1,11 +1,11 @@
 'use strict'
 /* global describe, it, expect */
-describe('Standard Text', function() {
-  it('should load', function() {
+describe('Standard Text', function () {
+  it('should load', function () {
     // assertions here]
     expect(typeof jsPDF, 'function')
   })
-  it('should allow text insertion', function() {
+  it('should allow text insertion', function () {
     var doc = new jsPDF()
     doc.text(10, 10, 'This is a test!')
     doc.output('dataurlnewwindow')
