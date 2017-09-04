083a407a5f20943d3b7b0159b16b46dff4797530
Aras Abbasi <aras.abbasi@googlemail.com>
2017-09-04 03:34:14 +0200

    Update standard.spec.js

--- tests/viewerpreferences/standard.spec.js
+++ tests/viewerpreferences/standard.spec.js
@@ -10,45 +10,45 @@ describe('viewerpreferences plugin', () => {
   it('HideToolbar', () => {
     const doc = jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerpreferences({'HideToolbar': true});
+    doc.viewerPreferences({'HideToolbar': true});
 
     comparePdf(doc.output(), 'HideToolbar.pdf', 'viewerpreferences')
   })
   it('HideMenubar', () => {
     const doc = jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerpreferences({'HideMenubar': true});
+    doc.viewerPreferences({'HideMenubar': true});
 
     comparePdf(doc.output(), 'HideMenubar.pdf', 'viewerpreferences')
   })
   it('HideWindowUI', () => {
     const doc = jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerpreferences({'HideWindowUI': true});
+    doc.viewerPreferences({'HideWindowUI': true});
 
     comparePdf(doc.output(), 'HideWindowUI.pdf', 'viewerpreferences')
   })
   it('FitWindow', () => {
     const doc = jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerpreferences({'FitWindow': true});
+    doc.viewerPreferences({'FitWindow': true});
 
     comparePdf(doc.output(), 'FitWindow.pdf', 'viewerpreferences')
   })
   it('check if reset works var. 1', () => {
     const doc = jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerpreferences({'HideWindowUI': true});
-    doc.viewerpreferences('reset');
-    doc.viewerpreferences({'FitWindow': true});
+    doc.viewerPreferences({'HideWindowUI': true});
+    doc.viewerPreferences('reset');
+    doc.viewerPreferences({'FitWindow': true});
     
     comparePdf(doc.output(), 'FitWindow.pdf', 'viewerpreferences')
   })
   it('check if reset works var. 2', () => {
     const doc = jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerpreferences({'HideWindowUI': true});
-    doc.viewerpreferences({'FitWindow': true}, true);
+    doc.viewerPreferences({'HideWindowUI': true});
+    doc.viewerPreferences({'FitWindow': true}, true);
     
     comparePdf(doc.output(), 'FitWindow.pdf', 'viewerpreferences')
   })
