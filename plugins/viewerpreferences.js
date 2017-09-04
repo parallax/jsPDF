29b80c682148658b5be517123e472fba59eb33f0
Aras Abbasi <aras.abbasi@googlemail.com>
2017-09-04 13:49:26 +0200

    Update viewerpreferences.js
    added missing values for ViewArea etc.
    
    Example for PrintPageRange is 
    doc.viewPreferences({'PrintPageRange': [[1,2], [3,4], [6,8], [9]]});
    
    http://blogs.datalogics.com/2016/06/23/recreating-acrobats-document-properties-advanced-tab/

--- plugins/viewerpreferences.js
+++ plugins/viewerpreferences.js
@@ -29,10 +29,10 @@
         "DisplayDocTitle"           : {defaultValue : false, value : false, type: 'boolean', explicitSet : false, valueSet : [true, false], pdfVersion : 1.4},
         "NonFullScreenPageMode"     : {defaultValue : 'UseNone', value : 'UseNone', type: 'name', explicitSet : false, valueSet : ['UseNone', 'UseOutlines', 'UseThumbs', 'UseOC'], pdfVersion : 1.3},
         "Direction"                 : {defaultValue : 'L2R', value : 'L2R', type: 'name', explicitSet : false, valueSet : ['L2R', 'R2L'], pdfVersion : 1.3},
-        "ViewArea"                  : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['CropBox'], pdfVersion : 1.4},
-        "ViewClip"                  : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['CropBox'], pdfVersion : 1.4},
-        "PrintArea"                 : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['CropBox'], pdfVersion : 1.4},
-        "PrintClip"                 : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['CropBox'], pdfVersion : 1.4},
+        "ViewArea"                  : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['MediaBox', 'CropBox', 'TrimBox', 'BleedBox', 'ArtBox'], pdfVersion : 1.4},
+        "ViewClip"                  : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['MediaBox', 'CropBox', 'TrimBox', 'BleedBox', 'ArtBox'], pdfVersion : 1.4},
+        "PrintArea"                 : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['MediaBox', 'CropBox', 'TrimBox', 'BleedBox', 'ArtBox'], pdfVersion : 1.4},
+        "PrintClip"                 : {defaultValue : 'CropBox', value : 'CropBox', type: 'name', explicitSet : false, valueSet : ['MediaBox', 'CropBox', 'TrimBox', 'BleedBox', 'ArtBox'], pdfVersion : 1.4},
         "PrintScaling"              : {defaultValue : 'AppDefault', value : 'AppDefault', type: 'name', explicitSet : false, valueSet : ['AppDefault', 'None'], pdfVersion : 1.6},
         "Duplex"                    : {defaultValue : 'none', value : 'none', type: 'name', explicitSet : false, valueSet : ['Simplex', 'DuplexFlipShortEdge', 'DuplexFlipLongEdge', 'none'], pdfVersion : 1.7},
         "PickTrayByPDFSize"         : {defaultValue : false, value : false, tyype: 'boolean', explicitSet : false, valueSet : [true, false], pdfVersion : 1.7},
@@ -109,8 +109,18 @@
 		configuration[method].value = value; 
 	} else if (configuration[method].type === "integer" && Number.isInteger(value)) {
 		configuration[method].value = value; 
-	} else if (configuration[method].type === "array" && (value.length % 2 === 0) && !value.some(isNaN)) {
-		configuration[method].value = value; 
+	} else if (configuration[method].type === "array") {
+		var rangeArray = [],
+			pdfArray = '';
+		for (var i = 0; i < value.length; i++) {
+			if (value[i].length === 1 && !value[i].some(isNaN)) {
+				rangeArray.push(String(value[i]));
+		    	} else if (value[i].length > 1  && !value[i].some(isNaN)){
+				rangeArray.push(String(value[i].join('-')));
+		    }
+		}
+		pdfArray = String(rangeArray);
+		configuration[method].value = pdfArray; 
 	} else {
 		configuration[method].value = configuration[method].defaultValue; 
 	}
