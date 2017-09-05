fb0f152f3f026b6b111aba317653762c8ff777b8
Aras Abbasi <aras.abbasi@googlemail.com>
2017-09-05 09:12:16 +0200

    Update standard.spec.js
    are semicolons the problem? do i have to initialize jsPDF?

--- tests/viewerpreferences/standard.spec.js
+++ tests/viewerpreferences/standard.spec.js
@@ -8,61 +8,61 @@
  */
 describe('viewerpreferences plugin', () => {
   it('HideToolbar', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'HideToolbar': true});
+    doc.viewerPreferences({'HideToolbar': true})
 
     comparePdf(doc.output(), 'HideToolbar.pdf', 'viewerpreferences')
   })
   it('HideMenubar', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'HideMenubar': true});
+    doc.viewerPreferences({'HideMenubar': true})
 
     comparePdf(doc.output(), 'HideMenubar.pdf', 'viewerpreferences')
   })
   it('HideWindowUI', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'HideWindowUI': true});
+    doc.viewerPreferences({'HideWindowUI': true})
 
     comparePdf(doc.output(), 'HideWindowUI.pdf', 'viewerpreferences')
   })
   it('FitWindow', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'FitWindow': true});
+    doc.viewerPreferences({'FitWindow': true})
 
     comparePdf(doc.output(), 'FitWindow.pdf', 'viewerpreferences')
   })
   it('check if reset works var. 1', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'HideWindowUI': true});
-    doc.viewerPreferences('reset');
-    doc.viewerPreferences({'FitWindow': true});
+    doc.viewerPreferences({'HideWindowUI': true})
+    doc.viewerPreferences('reset')
+    doc.viewerPreferences({'FitWindow': true})
     
     comparePdf(doc.output(), 'FitWindow.pdf', 'viewerpreferences')
   })
   it('check if reset works var. 2', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'HideWindowUI': true});
-    doc.viewerPreferences({'FitWindow': true}, true);
+    doc.viewerPreferences({'HideWindowUI': true})
+    doc.viewerPreferences({'FitWindow': true}, true)
     
     comparePdf(doc.output(), 'FitWindow.pdf', 'viewerpreferences')
   })
   it('ViewArea MediaBox', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'ViewArea' : 'MediaBox'});
+    doc.viewerPreferences({'ViewArea' : 'MediaBox'})
     
     comparePdf(doc.output(), 'ViewAreaMediaBox.pdf', 'viewerpreferences')
   })
   it('PrintPageRange', () => {
-    const doc = jsPDF()
+    const doc = new jsPDF()
     doc.text(10, 10, 'This is a test')
-    doc.viewerPreferences({'PrintPageRange' : [[1,3],[5,9]]});
+    doc.viewerPreferences({'PrintPageRange' : [[1,3],[5,9]]})
     
     comparePdf(doc.output(), 'PrintPageRange.pdf', 'viewerpreferences')
   })
