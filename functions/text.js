a5ec65c803ac7e7c49cfcb0a112866b888277cca
Aras Abbasi <aras.abbasi@googlemail.com>
2017-09-09 14:24:47 +0200

    Update text.js

--- functions/text.js
+++ functions/text.js
@@ -45,6 +45,19 @@
 				}
 				options = {flags: flags, angle: angle, align: align};
 			}
+			// If there are any newlines in text, we assume
+			// the user wanted to print multiple lines, so break the
+			// text up into an array.  If the text is already an array,
+			// we assume the user knows what they are doing.
+			// Convert text into an array anyway to simplify
+			// later code.
+			if (typeof text === 'string') {
+			  if (text.match(/[\n\r]/)) {
+				text = text.split(/\r\n|\r|\n/g);
+			  } else {
+				text = [text];
+			  }
+			}
 			return {
 				text : text,
 				x : x,
@@ -185,10 +198,112 @@
 				y : y,
 				options: options,
 				mutual: mutual
-			}
+			};
 		}
 		
 		jsPDF.FunctionsPool.text.push(
 			renderingModeFunction
+		);
+		
+		var alignFunction = function (args) {
+			var text = args.text;
+			var x = args.x;
+			var y = args.y;
+			var options = args.options || {};
+			var mutual = args.mutual || {};
+			
+			var align = options.align; 
+			var leading = mutual.leading;
+			var lineWidth = mutual.lineWidth;
+			var k = mutual.k;
+			
+			var lineWidths = text.map(function(v) {
+					return mutual.scope.getStringUnitWidth(v) * mutual.activeFontSize / k;
+				}, mutual.scope)
+				
+			var flags = {};
+			if (!('noBOM' in options))
+			  flags.noBOM = true;
+			if (!('autoencode' in options))
+			  flags.autoencode = true;
+			
+        function ESC(s, options) {
+			options = options || {};
+          s = s.split("\t").join(Array(options.TabLen || 9).join(" "));
+		  return s;
+          return mutual.pdfEscape(s, flags);
+        }
+		
+        if (typeof text === 'string') {
+          text = ESC(text, options);
+        } else if (Object.prototype.toString.call(text) ===
+          '[object Array]') {
+          // we don't want to destroy  original text array, so cloning it
+          var sa = text.concat(),
+            da = [],
+            len = sa.length;
+          // we do array.join('text that must not be PDFescaped")
+          // thus, pdfEscape each component separately
+          while (len--) {
+            da.push(ESC(sa.shift(), options));
+          }
+          var linesLeft = Math.ceil((mutual.pageHeight - y - mutual.runningPageHeight) *
+            k / (mutual.activeFontSize * mutual.lineHeightProportion));
+          if (0 <= linesLeft && linesLeft < da.length + 1) {
+            //todo = da.splice(linesLeft-1);
+          }          
+			
+		if (align) {
+            var left,
+              prevX,
+              maxLineLength;
+            maxLineLength = Math.max.apply(Math, lineWidths);
+            // The first line uses the "main" Td setting,
+            // and the subsequent lines are offset by the
+            // previous line's x coordinate.
+            if (align === "center") {
+              // The passed in x coordinate defines
+              // the center point.
+              left = x - maxLineLength / 2;
+              x -= lineWidths[0] / 2;
+            } else if (align === "right") {
+              // The passed in x coordinate defines the
+              // rightmost point of the text.
+              left = x - maxLineLength;
+              x -= lineWidths[0];
+            } else {
+              throw new Error(
+                'Unrecognized alignment option, use "center" or "right".'
+              );
+            }
+            prevX = x;
+            text = da[0];
+            for (var i = 1, len = da.length; i < len; i++) {
+              var delta = maxLineLength - lineWidths[i];
+              if (align === "center") delta /= 2;
+              // T* = x-offset leading Td ( text )
+              text += ") Tj\n" + ((left - prevX) + delta) + " -" + leading +
+                " Td (" + da[i];
+              prevX = left + delta;
+            }
+          } else {
+            text = da.join(") Tj\nT* (");
+          }
+			return {
+				text : text,
+				x : x,
+				y : y,
+				options: options,
+				mutual: mutual
+			};
+        } else {
+          throw new Error('Type of text must be string or Array. "' + text +
+            '" is not recognized.');
+        }
+		}
+		
+		
+		jsPDF.FunctionsPool.text.push(
+			alignFunction
 		);
 })(jsPDF.API);
