5bff7469d9a83b498c11cd2d461357b9b31d4552
Aras Abbasi <aras.abbasi@googlemail.com>
2017-10-29 17:58:40 +0100

    Update compare.js

--- tests/utils/compare.js
+++ tests/utils/compare.js
@@ -4,6 +4,7 @@ function loadBinaryResource (url) {
   const req = new XMLHttpRequest()
   req.open('GET', url, false)
    // XHR binary charset opt by Marcus Granado 2006 [http://mgran.blogspot.com]
+  req.overrideMimeType('text\/plain; charset=x-user-defined');
   req.responseType = "arraybuffer";
   req.send(null)
   if (req.status !== 200) {
@@ -43,4 +44,38 @@ window.comparePdf = (actual, expectedFile, suite) => {
   actual = resetCreationDate(actual.trim())
 
   expect(actual).toEqual(expected)
+}
+
+window.comparePdf = (actual, expectedFile, suite) => {
+    let pdf;
+    let ready = false;
+    let result = '';
+
+    let check = function() {
+        if (ready === true) {
+              const expected = resetCreationDate(pdf).trim()
+              actual = resetCreationDate(actual.trim())
+
+              expect(actual).toEqual(expected)
+             return;
+        }
+        setTimeout(check, 1000);
+    }
+
+    check();
+
+    const req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.responseType = "arraybuffer";
+    req.onloadend = function(evt) {
+        // file is loaded
+        pdf = evt.target.result;
+        ready = true;
+    };
+    req.onerror = function () {
+        sendReference(`/tests/${suite}/reference/${expectedFile}`, resetCreationDate(actual))
+        pdf = actual
+        ready = true;
+    }
+    req.send(null)
 }
