decfebc9675103cdb3b218b0975038a69828e3c5
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-30 10:55:24 +0900

    add customfonts test

--- saucelabs.karma.conf.js
+++ saucelabs.karma.conf.js
@@ -67,7 +67,7 @@ module.exports = (config) => {
         pattern: 'tests/**/*.spec.js',
         included: true
       }, {
-        pattern: 'tests/**/reference/*.pdf',
+        pattern: 'tests/**/reference/*.*',
         included: false,
         served: true
       }
