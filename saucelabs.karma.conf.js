e0a71ee5ef19408e894e5f527709619dd6e86160
Aras Abbasi <aras.abbasi@googlemail.com>
2018-01-10 20:47:37 +0100

    fix possible merging error

--- saucelabs.karma.conf.js
+++ saucelabs.karma.conf.js
@@ -60,6 +60,7 @@ module.exports = (config) => {
       'plugins/standard_fonts_metrics.js',
       'plugins/autoprint.js',
       'plugins/addhtml.js',
+      'plugins/viewerpreferences.js',
       'plugins/setlanguage.js',
       'tests/utils/compare.js',
       {
