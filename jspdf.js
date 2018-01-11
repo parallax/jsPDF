a9b299edfe6f388f1c4e513b85ae7464da509141
Cyd La Luz <claluz@tesla.com>
2018-01-11 11:25:50 -0800

    Convert hex concatenation in getTextColor to a one liner and index update

--- jspdf.js
+++ jspdf.js
@@ -1117,19 +1117,17 @@ var jsPDF = (function(global) {
         return activeFontSize;
       },
       'getTextColor': function getTextColor() {
-        var colorEncoded = textColor.split(' ')
-        if (colorEncoded.length == 2 && colorEncoded[colorEncoded.length-1] == 'g') {
+        var colorEncoded = textColor.split(' ');
+        if (colorEncoded.length === 2 && colorEncoded[1] === 'g') {
           // convert grayscale value to rgb so that it can be converted to hex for consistency
-          var floatVal = parseFloat(colorEncoded[0])
-          colorEncoded = [floatVal, floatVal, floatVal, 'r']
+          var floatVal = parseFloat(colorEncoded[0]);
+          colorEncoded = [floatVal, floatVal, floatVal, 'r'];
         }
-        var x;
-        var colorAsHex = '#'
+        var colorAsHex = '#';
         for (var i = 0; i < 3; i++) {
-          x = Math.floor(parseFloat(colorEncoded[i]) * 255).toString(16);
-          colorAsHex += (x.length == 1) ? "0"+x : x;
+          colorAsHex += ('0' + Math.floor(parseFloat(colorEncoded[i]) * 255).toString(16)).slice(-2);
         }
-        return colorAsHex
+        return colorAsHex;
       },
       'getLineHeight': function() {
         return activeFontSize * lineHeightProportion;
