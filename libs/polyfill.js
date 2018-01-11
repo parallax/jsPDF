6ceee5a51816813a8599b32b6dc0eca06503b7fa
Aras Abbasi <aras.abbasi@googlemail.com>
2018-01-11 13:46:07 +0100

    Update polyfill.js

--- libs/polyfill.js
+++ libs/polyfill.js
@@ -192,5 +192,26 @@
 			return this.replace(/\s+$/g, "");
 		};
 	}
+	//https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+	if (typeof Object.assign != 'function') {
+	  Object.assign = function(target) {
+	    'use strict';
+	    if (target == null) {
+	      throw new TypeError('Cannot convert undefined or null to object');
+	    }
 
+	    target = Object(target);
+	    for (var index = 1; index < arguments.length; index++) {
+	      var source = arguments[index];
+	      if (source != null) {
+		for (var key in source) {
+		  if (Object.prototype.hasOwnProperty.call(source, key)) {
+		    target[key] = source[key];
+		  }
+		}
+	      }
+	    }
+	    return target;
+	  };
+	}
 })(typeof self !== "undefined" && self || typeof window !== "undefined" && window || this);
