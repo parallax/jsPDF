da2da23038a0c58b975a7143ecd7101635352820
Aras Abbasi <aras.abbasi@googlemail.com>
2018-02-15 00:28:27 +0100

    Update ttfsupport.js

--- plugins/ttfsupport.js
+++ plugins/ttfsupport.js
@@ -3,16 +3,18 @@
  * Licensed under the MIT License.
  * http://opensource.org/licenses/mit-license
  */
-(function (jsPDFAPI) {
+(function (jsPDFAPI, global) {
     "use strict";
 
     jsPDFAPI.events.push([ 
     	'addFont'
     	,function(font) {
             if (jsPDFAPI.existsFileInVFS(font.postScriptName)) {
-                font.metadata = TTFFont.open(font.postScriptName, font.fontName, jsPDFAPI.getFileFromVFS(font.postScriptName), font.encoding);
+                font.metadata = global.TTFFont.open(font.postScriptName, font.fontName, jsPDFAPI.getFileFromVFS(font.postScriptName), font.encoding);
                 font.metadata.Unicode = font.metadata.Unicode || {encoding: {}, kerning: {}, widths: []};
+            } else if (font.id.slice(1) >= 14) {
+                console.error("Font does not exist in FileInVFS, import fonts or remove declaration doc.addFont('" + font.postScriptName + "').");
             }
     	}
     ]) // end of adding event handler
-})(jsPDF.API);
+})(jsPDF.API, typeof self !== "undefined" && self || typeof global !== "undefined" && global || typeof window !== "undefined" && window || (Function ("return this"))());
