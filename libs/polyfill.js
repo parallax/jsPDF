aff2493c3c4777d5af2dc4dd388299109547fa5a
Aras Abbasi <aras.abbasi@googlemail.com>
2018-02-18 01:22:15 +0100

    Update polyfill.js

--- libs/polyfill.js
+++ libs/polyfill.js
@@ -249,8 +249,7 @@
         };
     }
 
-
-})(typeof self !== "undefined" && self || typeof window !== "undefined" && window || typeof global !== "undefined" && global || Function('return this.content')() || Function('return this')());
+}(typeof self !== "undefined" && self || typeof window !== "undefined" && window || typeof global !== "undefined" && global ||  Function('return typeof this === "object" && this.content')() || Function('return this')()));
 // `self` is undefined in Firefox for Android content script context
 // while `this` is nsIContentFrameMessageManager
 // with an attribute `content` that corresponds to the window
