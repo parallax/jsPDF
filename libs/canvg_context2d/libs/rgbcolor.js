9ac057586df659f7ed73f1829b05e6132aea0325
Aras Abbasi <aras.abbasi@googlemail.com>
2018-02-22 19:41:24 +0100

    Update rgbcolor.js

--- libs/canvg_context2d/libs/rgbcolor.js
+++ libs/canvg_context2d/libs/rgbcolor.js
@@ -300,5 +300,8 @@
     }
 
     global.RGBColor = RGBColor;
-
-}( typeof window !== 'undefined' ? window : this ));
+	
+}(typeof self !== "undefined" && self || typeof window !== "undefined" && window || typeof global !== "undefined" && global ||  Function('return typeof this === "object" && this.content')() || Function('return this')()));
+// `self` is undefined in Firefox for Android content script context
+// while `this` is nsIContentFrameMessageManager
+// with an attribute `content` that corresponds to the window
