e8a68ec8bd84ef7e97a9fbdb287a5bfa784b51cf
Aras Abbasi <aras.abbasi@googlemail.com>
2018-02-22 19:42:15 +0100

    Update StackBlur.js

--- libs/canvg_context2d/libs/StackBlur.js
+++ libs/canvg_context2d/libs/StackBlur.js
@@ -651,5 +651,8 @@ OTHER DEALINGS IN THE SOFTWARE.
 	}
 
 	global.stackBlur = stackBlur;
-
-}( typeof window !== 'undefined' ? window : this ));
+	
+}(typeof self !== "undefined" && self || typeof window !== "undefined" && window || typeof global !== "undefined" && global ||  Function('return typeof this === "object" && this.content')() || Function('return this')()));
+// `self` is undefined in Firefox for Android content script context
+// while `this` is nsIContentFrameMessageManager
+// with an attribute `content` that corresponds to the window
