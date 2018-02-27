aa53c707f6dbc1232260309a4014aa986002f37f
Aras Abbasi <aras.abbasi@googlemail.com>
2018-02-27 09:55:20 +0100

    Update acroform.js

--- plugins/acroform.js
+++ plugins/acroform.js
@@ -45,7 +45,7 @@
     */
     var getStringUnitWidth = function(text, options) {
         var result = 0;
-        if (typeof TTFFont === "function" && options.font.metadata instanceof TTFFont === true) {
+        if (typeof options.font.metadata.widthOfString === "function") {
             result = options.font.metadata.widthOfString(text, options.fontSize, options.charSpace);
         } else {
             result = getArraySum(getCharWidthsArray(text, options)) * options.fontSize;
