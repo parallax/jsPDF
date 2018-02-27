8409afb05f524c4e50f52a3b88b97b06cc38ddf8
Aras Abbasi <aras.abbasi@googlemail.com>
2018-02-27 09:51:15 +0100

    Update ttfsupport.js

--- plugins/ttfsupport.js
+++ plugins/ttfsupport.js
@@ -3,18 +3,18 @@
  * Licensed under the MIT License.
  * http://opensource.org/licenses/mit-license
  */
-(function (jsPDFAPI, global) {
+(function (jsPDF, global) {
     "use strict";
 
-    jsPDFAPI.events.push([ 
+    jsPDF.API.events.push([ 
     	'addFont'
     	,function(font) {
-            if (jsPDFAPI.existsFileInVFS(font.postScriptName)) {
-                font.metadata = global.TTFFont.open(font.postScriptName, font.fontName, jsPDFAPI.getFileFromVFS(font.postScriptName), font.encoding);
+            if (jsPDF.API.existsFileInVFS(font.postScriptName)) {
+                font.metadata = jsPDF.API.TTFFont.open(font.postScriptName, font.fontName, jsPDF.API.getFileFromVFS(font.postScriptName), font.encoding);
                 font.metadata.Unicode = font.metadata.Unicode || {encoding: {}, kerning: {}, widths: []};
             } else if (font.id.slice(1) >= 14) {
                 console.error("Font does not exist in FileInVFS, import fonts or remove declaration doc.addFont('" + font.postScriptName + "').");
             }
     	}
     ]) // end of adding event handler
-})(jsPDF.API, typeof self !== "undefined" && self || typeof global !== "undefined" && global || typeof window !== "undefined" && window || (Function ("return this"))());
+})(jsPDF, typeof self !== "undefined" && self || typeof global !== "undefined" && global || typeof window !== "undefined" && window || (Function ("return this"))());
