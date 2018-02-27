69bc08ee36ac4e15ccc6e764bd5039d09b6b0302
Aras Abbasi <aras.abbasi@googlemail.com>
2018-02-27 19:36:41 +0100

    Update compare.js

--- tests/utils/compare.js
+++ tests/utils/compare.js
@@ -2,10 +2,10 @@
 var globalVar = (typeof self !== "undefined" && self || typeof global !== "undefined" && global || typeof window !== "undefined" && window || (Function ("return this"))());
 function loadBinaryResource (url, unicodeCleanUp) {
   const req = new XMLHttpRequest()
-  req.open('GET', url, false)
+  req.open('GET', url, false);
    // XHR binary charset opt by Marcus Granado 2006 [http://mgran.blogspot.com]
   req.overrideMimeType('text\/plain; charset=x-user-defined');
-  req.send(null)
+  req.send(null);
   if (req.status !== 200) {
     throw new Error('Unable to load file');
   }
@@ -29,15 +29,15 @@ function cleanUpUnicode(value) {
 }
 
 function sendReference (filename, data) {
-  var req = new XMLHttpRequest()
-  req.open('POST', 'http://localhost:9090/'+filename, true)
-  req.send(data)
+  var req = new XMLHttpRequest();
+  req.open('POST', 'http://localhost:9090/'+filename, true);
+  req.send(data);
 }
 
 function resetFile(pdfFile) {
-  pdfFile.replace(/\/CreationDate \(D:(.*?)\)/, '/CreationDate (D:19871210000000+00\'00\'\)');
-  pdfFile.replace(/(\/ID \[ (<0[0-9a-fA-F]+> ){2}\])/, '/ID [ <00000000000000000000000000000000> <00000000000000000000000000000000> ]');
-  pdfFile.replace(/(\/Producer \(jsPDF [1-9].[0-9].[0-9]\))/, '/Producer (jsPDF 1.0.0)');
+  pdfFile = pdfFile.replace(/\/CreationDate \(D:(.*?)\)/, '/CreationDate (D:19871210000000+00\'00\'\)');
+  pdfFile = pdfFile.replace(/(\/ID \[ (<[0-9a-fA-F]+> ){2}\])/, '/ID [ <00000000000000000000000000000000> <00000000000000000000000000000000> ]');
+  pdfFile = pdfFile.replace(/(\/Producer \(jsPDF [1-9].[0-9].[0-9]\))/, '/Producer (jsPDF 1.0.0)');
   return pdfFile;
 }
 /**
@@ -52,7 +52,7 @@ globalVar.comparePdf = function (actual, expectedFile, suite, unicodeCleanUp) {
   try {
     pdf = loadBinaryResource('/base/tests/' + suite + '/reference/' + expectedFile, unicodeCleanUp);
   } catch (error) {
-	console.log("Error loading '/base/tests/" + suite + "/reference/" + expectedFile + "'");
+    console.log("Error loading '/base/tests/" + suite + "/reference/" + expectedFile + "'");
     sendReference('/tests/${suite}/reference/' + expectedFile, resetFile(actual))
     pdf = actual;
   }
