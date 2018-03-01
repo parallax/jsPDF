7e4dcc279ccd29211090b8f8bc209bfe51609432
Aras Abbasi <aras.abbasi@googlemail.com>
2018-03-01 02:58:26 +0100

    Update saucelabs.karma.conf.js

--- saucelabs.karma.conf.js
+++ saucelabs.karma.conf.js
@@ -65,7 +65,7 @@ module.exports = (config) => {
       'libs/ttffont.js',
       'tests/utils/compare.js',
       {
-        pattern: 'tests/addimage/jpeg.spec.js',
+        pattern: 'tests/addimage/*.spec.js',
         included: true
       }, {
         pattern: 'tests/**/reference/*.pdf',
