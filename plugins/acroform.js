da9aefec98a726acd61e4478cd884b3f23636b20
Aras Abbasi <aras.abbasi@googlemail.com>
2018-05-27 10:27:24 +0200

    Update acroform.js

--- plugins/acroform.js
+++ plugins/acroform.js
@@ -1758,4 +1758,17 @@
         jsPDFAPI.AcroFormCheckBox = AcroFormCheckBox;
         jsPDFAPI.AcroFormTextField = AcroFormTextField;
         jsPDFAPI.AcroFormPasswordField = AcroFormPasswordField;
+        
+        jsPDFAPI.AcroForm = {
+            ChoiceField : AcroFormChoiceField,
+            ListBox : AcroFormListBox,
+            ComboBox : AcroFormComboBox,
+            EditBox : AcroFormEditBox,
+            Button : AcroFormButton,
+            PushButton : AcroFormPushButton,
+            RadioButton : AcroFormRadioButton,
+            CheckBox : AcroFormCheckBox,
+            TextField : AcroFormTextField,
+            PasswordField : AcroFormPasswordField
+        };
 })(jsPDF.API, (typeof window !== "undefined" && window || typeof global !== "undefined" && global));
