9bb1fe8a31e5f187d806d2c6018e0781e396ae41
Aras Abbasi <aras.abbasi@googlemail.com>
2018-06-05 22:57:17 +0200

    Update polyfillEnforcer.js

--- tests/utils/polyfillEnforcer.js
+++ tests/utils/polyfillEnforcer.js
@@ -9,10 +9,10 @@ delete Array.isArray;
 delete Array.prototype.forEach;
 delete Object.keys;
 delete Object.assign;
+**/
 delete String.prototype.trim;
 delete String.prototype.trimLeft;
 delete String.prototype.trimRight;
-**/
 
 }(typeof self !== "undefined" && self || typeof window !== "undefined" && window || typeof global !== "undefined" && global ||  Function('return typeof this === "object" && this.content')() || Function('return this')()));
 // `self` is undefined in Firefox for Android content script context
