35e3eb234f14a794c9bd2141a542579a259f482c
Aras Abbasi <aras.abbasi@googlemail.com>
2018-06-06 00:35:51 +0200

    Update saucelabs.karma.conf.js

--- saucelabs.karma.conf.js
+++ saucelabs.karma.conf.js
@@ -65,6 +65,7 @@ module.exports = (config) => {
       'libs/png_support/png.js',
       'libs/png_support/zlib.js',
       'node_modules/omggif/omggif.js',
+      'node_modules/html2canvas/dist/html2canvas.js',
       'libs/BMPDecoder.js',
       'libs/ttffont.js',
       'libs/rgbcolor.js',
