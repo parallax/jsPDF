1081fad7d024c481406b237cde1923419148fdca
Aras Abbasi <aras.abbasi@googlemail.com>
2018-09-01 01:32:36 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -95,10 +95,9 @@ module.exports = (config) => {
     },
     babelPreprocessor: {
       options: {
-        presets: ['env'],
+        presets: ["@babel/env"], // "@babel/preset-env"
         sourceMap: 'inline'
       }
     }
-
   })
 }
