f348893b93ff98cb5e7649a387d4be025e2c84c8
Aras Abbasi <aras.abbasi@googlemail.com>
2018-08-31 23:03:17 +0200

    Update Uint8Array.spec.js

--- tests/addimage/Uint8Array.spec.js
+++ tests/addimage/Uint8Array.spec.js
@@ -6,9 +6,9 @@
 
 describe('Plugin: addimage filetypeRecognition', () => {
 
-  var BASE64_MARKER = ';base64,';
 
   function convertDataURIToBinary(dataURI) {
+    var BASE64_MARKER = ';base64,';
     var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
     var base64 = dataURI.substring(base64Index);
     var raw = window.atob(base64);
@@ -22,7 +22,7 @@ describe('Plugin: addimage filetypeRecognition', () => {
   }
   
   var jpgImage = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD+f+iiigD/2Q==';
-  let jpgImageUint8Array = convertDataURIToBinary(jpgImage);
+  var jpgImageUint8Array = convertDataURIToBinary(jpgImage);
   
 	it('Uint8Array: getImageFileTypeByImageData', () => {
     expect(jsPDF.API.getImageFileTypeByImageData(jpgImageUint8Array)).toEqual("JPEG");
