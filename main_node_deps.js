b1a47058a7abda93a97a814de49642abcd12690d
Aras Abbasi <aras.abbasi@googlemail.com>
2018-09-01 19:54:16 +0430

    Add files via upload

--- main_node_deps.js
+++ main_node_deps.js
@@ -1,4 +1,9 @@
 import './node_modules/cf-blob.js/Blob.js';
 import './node_modules/file-saver/FileSaver.js';
 import './node_modules/omggif/omggif.js';
-import './libs/html2canvas/dist/html2canvas.js';
+import './node_modules/html2canvas/dist/html2canvas.js';
+
+//patch for html2canvas
+(function ( globalObj) {
+	globalObj.html2canvas = exports.html2canvas;
+} (typeof window !== "undefined" && window || typeof global !== "undefined" && global));
