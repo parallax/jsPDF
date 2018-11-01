1ea40e4012457e748e69972e34cbc6596d1d1c43
Aras Abbasi <aras.abbasi@googlemail.com>
2018-11-01 23:46:51 +0100

    Refactor arabic parser, Add Bidi Engine (#2041)
    * Add files via upload
    
    * Add files via upload
    
    * Update main_plugins.js
    
    * Add files via upload
    
    * Add files via upload
    
    * Update bidiEngine.spec.js
    
    * Update arabic.js

--- tests/arabic/processArabic.spec.js
+++ tests/arabic/processArabic.spec.js
@@ -1,8 +1,70 @@
+describe('arabicParser', function() {
 
-/* global describe, it, expect, jsPDF, comparePdf */
+  it('isArabicLetter', function() {
+    expect(jsPDF.API.__arabicParser__.isArabicLetter("ف")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.isArabicLetter("a")).toEqual(false);
+  });
+  
+  it('isArabicEndLetter', function() {
+    expect(jsPDF.API.__arabicParser__.isArabicEndLetter("د")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.isArabicEndLetter("ف")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicEndLetter("a")).toEqual(false);
+  });
+  
+  it('isArabicAlfLetter', function() {
+    expect(jsPDF.API.__arabicParser__.isArabicAlfLetter("ا")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.isArabicAlfLetter("د")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicAlfLetter("ف")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicAlfLetter("a")).toEqual(false);
+  });
+  
+  it('arabicLetterHasIsolatedForm', function() {
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasIsolatedForm("ا")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasIsolatedForm("د")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasIsolatedForm("ف")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasIsolatedForm("a")).toEqual(false);
+  });
 
-describe('processArabic', () => {
-  it('ف', () => {
+  it('arabicLetterHasFinalForm', function() {
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasFinalForm("ا")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasFinalForm("د")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasFinalForm("ف")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasFinalForm("a")).toEqual(false);
+  });
+
+  it('arabicLetterHasInitialForm', function() {
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasInitialForm("ا")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasInitialForm("د")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasInitialForm("ف")).toEqual(true);
+    //expect(jsPDF.API.__arabicParser__.arabicLetterHasInitialForm("ٌ")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasInitialForm("a")).toEqual(false);
+  })
+  
+  it('arabicLetterHasMedialForm', function() {
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasMedialForm("ا")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasMedialForm("د")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasMedialForm("ف")).toEqual(true);
+    //expect(jsPDF.API.__arabicParser__.arabicLetterHasMedialForm("ٌ")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.arabicLetterHasMedialForm("a")).toEqual(false);
+  });
+  
+  it('isArabicDiacritic', function() {
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("ا")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("د")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("ف")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("ٌ")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("a")).toEqual(false);
+  });
+  
+  it('getCorrectForm', function() {
+    expect(jsPDF.API.__arabicParser__.getCorrectForm("a", "","")).toEqual(-1);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("د")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("ف")).toEqual(false);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("ٌ")).toEqual(true);
+    expect(jsPDF.API.__arabicParser__.isArabicDiacritic("a")).toEqual(false);
+  });
+  
+  it('feh', function() {
 
     expect(jsPDF.API.processArabic("ف").charCodeAt(0)).toEqual(65233);
     expect(jsPDF.API.processArabic("دف").charCodeAt(1)).toEqual(65233);
@@ -25,10 +87,9 @@ describe('processArabic', () => {
     expect(jsPDF.API.processArabic("فففف").charCodeAt(1)).toEqual(65236);
     expect(jsPDF.API.processArabic("فففف").charCodeAt(2)).toEqual(65236);
     expect(jsPDF.API.processArabic("فففف").charCodeAt(3)).toEqual(65234);
-
-  })
+  });
   
-  it('د', () => {
+  it('dal', function() {
 
     expect(jsPDF.API.processArabic("د").charCodeAt(0)).toEqual(65193);  
     
@@ -49,9 +110,15 @@ describe('processArabic', () => {
     expect(jsPDF.API.processArabic("ددد").charCodeAt(0)).toEqual(65193);
     expect(jsPDF.API.processArabic("ددد").charCodeAt(1)).toEqual(65193);
     expect(jsPDF.API.processArabic("ددد").charCodeAt(2)).toEqual(65193);
-  })
+  });
   
-  it('ل ا', () => {
+  it('resolveLigatures', function() {
+    expect(jsPDF.API.__arabicParser__.resolveLigatures("ﻟﺎ").charCodeAt(0)).toEqual(65275);
+    expect(jsPDF.API.__arabicParser__.resolveLigatures("كاﻟﺎ").charCodeAt(2)).toEqual(65275);
+  });
+  
+  
+  it('lam alif', function() {
     
     expect(jsPDF.API.processArabic("لا").charCodeAt(0)).toEqual(65275);
     
@@ -71,8 +138,11 @@ describe('processArabic', () => {
     expect(jsPDF.API.processArabic("دلاا").charCodeAt(2)).toEqual(65165);
   })
   
-  
-  xit('ligatures', () => {
+  it("alif", function() {
+    expect(jsPDF.API.processArabic("اَ").charCodeAt(0)).toEqual(65165);
+  });
+ 
+  it('ligatures', function() {
     expect(jsPDF.API.processArabic("الله").charCodeAt(0)).toEqual(65010);
-  })
-})
+  });
+});
