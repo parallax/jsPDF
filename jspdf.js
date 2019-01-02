0a88bbce3654b156096cda25cd97d1c224619ff4
Julian Kern <kern.julian@gmx.de>
2019-01-02 09:16:44 +0100

    backwards compatibility to MrRio/jsPDF see comments on #15

--- jspdf.js
+++ jspdf.js
@@ -1711,8 +1711,14 @@ var jsPDF = (function(global) {
         });
       },
       output = SAFE(function(type, options) {
-        options = options || {};
-        options.filename = options.filename || "generated.pdf";
+        if (typeof options === "string") {
+          options = {
+            filename: options
+          };
+        } else {
+          options = options || {};
+          options.filename = options.filename || 'generated.pdf';
+        }
         var datauri =
           ("" + type).substr(0, 6) === "dataur"
             ? "data:application/pdf;filename=" + options.filename + ";base64," + btoa(buildDocument())
@@ -1727,7 +1733,7 @@ var jsPDF = (function(global) {
                 return API.output("dataurlnewwindow");
               }
             }
-            saveAs(getBlob(), options.filename);
+            saveAs(getBlob(), options);
             if (typeof saveAs.unload === "function") {
               if (global.setTimeout) {
                 setTimeout(saveAs.unload, 911);
@@ -4244,7 +4250,7 @@ var jsPDF = (function(global) {
      * @returns {jsPDF} jsPDF-instance
      */
     API.save = function(filename) {
-      API.output("save", {filename: filename});
+      API.output("save");
     };
 
     // applying plugins (more methods) ON TOP of built-in API.
