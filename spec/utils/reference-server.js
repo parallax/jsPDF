7b4ce7dddd136fa33228e1c4cbb645bed9eb3217
Lukas Hollaender <lukas.hollaender@yworks.com>
2020-07-02 18:14:32 +0200

    fix reference server (unicode) + nicer output

--- spec/utils/reference-server.js
+++ spec/utils/reference-server.js
@@ -1,30 +1,41 @@
 /* eslint-disable no-console */
-"use strict";
+'use strict'
 
 /**
  * The reference server collects and saves reference PDFs for the tests.
  */
-const http = require("http");
-const PORT = 9090;
-const fs = require("fs");
+const http = require('http')
+const PORT = 9090
+const fs = require('fs')
+const logUtils = require('log-utils')
+const chalk = require('chalk')
 
 // Create a server
 const server = http.createServer((request, response) => {
-  console.log(request.url);
+  const wstream = fs.createWriteStream(`./${request.url}`)
 
-  const wstream = fs.createWriteStream('./' + request.url, { flags: 'w', encoding: 'binary' })
-  console.log('Creating reference PDF ' + request.url + '.')
-  request.on('data', (chunk) => {
-    //console.log(chunk.length)
-    wstream.write(chunk.toString());
+  wstream.on('error', () => {
+    wstream.close()
+    console.log(
+        `${logUtils.timestamp} ${logUtils.error} ` +
+        chalk.red(`Could not write to file '${request.url}'. Is it locked by another process?`)
+    )
+  })
+
+  request.on('data', chunk => {
+    wstream.write(chunk)
   })
   request.on('end', () => {
     wstream.end()
+    console.log(logUtils.timestamp + ' ' + logUtils.ok(`Created reference '${request.url}'`))
   })
-  response.end('Test has sent reference PDF for ' + request.url)
+  response.writeHead(200, {
+    'Access-Control-Allow-Origin': '*'
+  })
+  response.end('Test has sent reference for ' + request.url)
 })
 
 // Lets start our server
 server.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
-});
+  console.log(`Server listening on: http://localhost:${PORT}`)
+})
