4ff277c21898d441ab0e65d60a2e68d41848ae43
Dhairya Gada <dhairyagada1997@gmail.com>
2020-10-28 11:59:15 +0530

    Angular Example Text

---
+++ examples/angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input'
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, 
+    MatButtonModule,
+    PdfViewerModule,
+    MatInputModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
