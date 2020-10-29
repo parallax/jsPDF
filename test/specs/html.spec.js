15264b06dbdc9a00a685af63d176138800db2ae0
Jeff Sieu <jeffsy00@gmail.com>
2020-10-29 17:08:59 +0800

    Add test cases for html margin, x and y

--- test/specs/html.spec.js
+++ test/specs/html.spec.js
@@ -14,4 +14,20 @@ describe("Module: html", function() {
     );
     comparePdf(doc.output(), "html-basic.pdf", "html");
   });
+
+  it("html margin insets properly", async () => {
+    const doc = jsPDF();
+    await new Promise(resolve =>
+      doc.html("<h1>Basic HTML</h1><h2>Heading 2</h2>", { callback: resolve, margin: [10, 10] })
+    );
+    comparePdf(doc.output(), "html-margin.pdf", "html");
+  });
+
+  it("html x, y offsets properly", async () => {
+    const doc = jsPDF();
+    await new Promise(resolve =>
+      doc.html("<h1>Basic HTML</h1><h2>Heading 2</h2>", { callback: resolve, x: 30, y: 40 })
+    );
+    comparePdf(doc.output(), "html-x-y.pdf", "html");
+  });
 });
