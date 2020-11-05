4f05e8c37066df1f694c9aef8260e2dc693a74c5
Lukas Hollaender <lukas.hollaender@yworks.com>
2020-11-05 16:27:49 +0100

    add a couple of test cases (results are still wrong)

--- test/specs/html.spec.js
+++ test/specs/html.spec.js
@@ -16,18 +16,105 @@ describe("Module: html", function() {
   });
 
   it("html margin insets properly", async () => {
-    const doc = jsPDF({ floatPrecision: 2 });
+    const doc = jsPDF({ floatPrecision: 2, unit: "pt" });
+    doc.line(30, 10, 100, 10);
+    doc.line(30, 10, 30, 100);
     await new Promise(resolve =>
-      doc.html("<h1>Basic HTML</h1><h2>Heading 2</h2>", { callback: resolve, margin: [10, 10] })
+      doc.html(
+        "<div style='background: red; width: 10px; height: 10px;'></div>",
+        {
+          callback: resolve,
+          margin: [10, 30]
+        }
+      )
     );
     comparePdf(doc.output(), "html-margin.pdf", "html");
   });
 
-  it("html x, y offsets properly", async () => {
-    const doc = jsPDF({ floatPrecision: 2 });
+  it("html margin on page break", async () => {
+    const doc = jsPDF({ floatPrecision: 2, unit: "pt" });
+    doc.rect(
+      30,
+      10,
+      doc.internal.pageSize.getWidth() - 60,
+      doc.internal.pageSize.getHeight() - 20
+    );
     await new Promise(resolve =>
-      doc.html("<h1>Basic HTML</h1><h2>Heading 2</h2>", { callback: resolve, x: 30, y: 40 })
+      doc.html(
+        "<div style='background: red; width: 10px; height: 1000px;'></div>",
+        {
+          callback: resolve,
+          margin: [10, 30, 10, 30]
+        }
+      )
+    );
+    doc.rect(
+      30,
+      10,
+      doc.internal.pageSize.getWidth() - 60,
+      doc.internal.pageSize.getHeight() - 20
+    );
+    comparePdf(doc.output(), "html-margin-page-break.pdf", "html");
+  });
+
+  it("html x, y offsets properly", async () => {
+    const doc = jsPDF({ floatPrecision: 2, unit: "pt" });
+    doc.line(30, 10, 100, 10);
+    doc.line(30, 10, 30, 100);
+    await new Promise(resolve =>
+      doc.html(
+        "<div style='background: red; width: 10px; height: 10px;'></div>",
+        {
+          callback: resolve,
+          x: 30,
+          y: 10
+        }
+      )
     );
     comparePdf(doc.output(), "html-x-y.pdf", "html");
+  });
+
+  it("html x, y + margin offsets properly", async () => {
+    const doc = jsPDF({ floatPrecision: 2, unit: "pt" });
+    doc.line(30, 10, 100, 10);
+    doc.line(30, 10, 30, 100);
+    await new Promise(resolve =>
+      doc.html(
+        "<div style='background: red; width: 10px; height: 10px;'></div>",
+        {
+          callback: resolve,
+          x: 10,
+          y: 3,
+          margin: [7, 20]
+        }
+      )
+    );
+    comparePdf(doc.output(), "html-margin-x-y.pdf", "html");
+  });
+
+  it("page break with text", async () => {
+    const doc = jsPDF({ floatPrecision: 2, unit: "pt" });
+    doc.rect(
+      30,
+      10,
+      doc.internal.pageSize.getWidth() - 60,
+      doc.internal.pageSize.getHeight() - 20
+    );
+    await new Promise(resolve =>
+      doc.html(
+        "<span>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</span>",
+        {
+          callback: resolve,
+          margin: [10, 30, 10, 30]
+        }
+      )
+    );
+    doc.rect(
+      30,
+      10,
+      doc.internal.pageSize.getWidth() - 60,
+      doc.internal.pageSize.getHeight() - 20
+    );
+    comparePdf(doc.output(), "html-margin-page-break-text.pdf", "html");
   });
 });
