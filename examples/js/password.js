b0141005e616e171cf60731c9e751604e6370d33
Rotzbua <Rotzbua@users.noreply.github.com>
2020-11-07 15:37:26 +0100

    Add security notice for encryption

--- examples/js/password.js
+++ examples/js/password.js
@@ -1,4 +1,7 @@
 var doc = new jsPDF({
+    // jsPDF supports encryption of PDF version 1.3.
+    // Version 1.3 just uses RC4 40-bit which is kown to be weak and is NOT state of the art.
+    // Keep in mind that it is just a minimal protection.
     encryption: {
         userPassword: "user",
         ownerPassword: "owner",
