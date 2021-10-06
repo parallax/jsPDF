cda5694e68c97898b835d4f957bb4b27211455d6
Megharth Lakhataria <m.c.lakhataria@gmail.com>
2021-10-06 05:24:00 -0400

    fix passing lineHeightFactor to the text method (#3283)
    Co-authored-by: Lukas Holländer <lukas.hollaender@yworks.com>

--- src/jspdf.js
+++ src/jspdf.js
@@ -3616,7 +3616,8 @@ function jsPDF(options) {
 
     //baseline
     var height = activeFontSize / scope.internal.scaleFactor;
-    var descent = height * (lineHeightFactor - 1);
+    var descent = height * (lineHeight - 1);
+
     switch (options.baseline) {
       case "bottom":
         y -= descent;
