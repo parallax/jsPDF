3d99b4a82091e76b9c870dce2e8135ed02216d00
Audun <audun.b.mo@protonmail.com>
2022-10-23 12:17:04 +0200

    fix: ensure malformatted data-URLs cant cause remote calls

--- test/specs/getImageProperties.spec.js
+++ test/specs/getImageProperties.spec.js
@@ -5,6 +5,15 @@
 
 describe("Module: addimage getImageProperties", () => {
   beforeAll(loadGlobals);
+	it("throws for malformatted data-URIs", () => {
+		const dataURI = `data:image/png;base64,${btoa("https://some-random-url.notreal/path?query=true")}`
+		const image = new Image()
+		image.src = dataURI
+		const t = () => {
+			jsPDF.API.getImageProperties(image)
+		}
+		expect(t).toThrowError("Supplied Data is not a valid base64-String jsPDF.convertBase64ToBinaryString ")
+	})
   it("one blackpixel", () => {
     var blackpixel =
       "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD+f+iiigD/2Q==";
