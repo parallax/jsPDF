1aee737f897bf5247148b31702bca833e0c99c19
Audun Bjørnerud Mo <audun.mo@remarkable.no>
2022-10-25 13:32:47 +0200

    fix: dont use Image constructor when running test in node

--- test/specs/getImageProperties.spec.js
+++ test/specs/getImageProperties.spec.js
@@ -6,9 +6,14 @@
 describe("Module: addimage getImageProperties", () => {
   beforeAll(loadGlobals);
 	it("throws for malformatted data-URIs", () => {
-		const dataURI = `data:image/png;base64,${btoa("https://some-random-url.notreal/path?query=true")}`
-		const image = new Image()
-		image.src = dataURI
+		const dataURL = `data:image/png;base64,${btoa("https://some-random-url.notreal/path?query=true")}`
+
+		const image = Image !== undefined ? new Image() : dataURL
+
+		// since <img> nodes cant be created in node, only create it when running browser tests. Otherwise, pass the dataURL directly
+		if (typeof image !== "string") {
+			image.src = dataURL
+		}
 		const t = () => {
 			jsPDF.API.getImageProperties(image)
 		}
