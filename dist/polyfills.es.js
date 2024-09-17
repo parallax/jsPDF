39778e9d6d3f7d0b3251598bfc4a0e520fe1f2f0
Lukas Hollaender <lukas.hollaender@yworks.com>
2024-09-17 15:30:16 +0200

    2.5.2

--- dist/polyfills.es.js
+++ dist/polyfills.es.js
@@ -1,7 +1,7 @@
 /** @license
  *
  * jsPDF - PDF Document creation from JavaScript
- * Version 2.5.1 Built on 2022-01-28T15:37:57.791Z
+ * Version 2.5.2 Built on 2024-09-17T13:29:57.860Z
  *                      CommitID 00000000
  *
  * Copyright (c) 2010-2021 James Hall <james@parall.ax>, https://github.com/MrRio/jsPDF
